d56f79b74532950f14beb0a4bd498cd9e51266b5
srdjanRakic <srgjan.rakic@gmail.com>
2018-10-08 11:35:53 +0200

    Configure flow and lint errors

--- styleguide/ThemeWrapper/index.js
+++ styleguide/ThemeWrapper/index.js
@@ -1,15 +1,19 @@
-import React, { Component } from "react";
+// @flow
+import React from "react";
+import type { Node } from "react";
 import { ThemeProvider, injectGlobal } from "styled-components";
 
-import buildGlobalStyles from "config/buildGlobalStyles";
-import configureFonts from "config/configureFonts";
-import theme from "config/theme";
+import buildGlobalStyles from "../../config/buildGlobalStyles";
+import configureFonts from "../../config/configureFonts";
+import theme from "../../config/theme";
 
 injectGlobal([buildGlobalStyles(theme)]);
 configureFonts(theme);
 
-export default class ThemeWrapper extends Component {
-  render() {
-    return <ThemeProvider theme={theme}>{this.props.children}</ThemeProvider>;
-  }
-}
+type Props = { children: Node };
+
+const ThemeWrapper = ({ children }: Props) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+export default ThemeWrapper;
