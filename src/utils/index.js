051a1cc16f12a44927ba4e4d01404ba7dd6480f4
srdjanRakic <srgjan.rakic@gmail.com>
2018-10-09 13:30:24 +0200

    Create font size/weight and colors util functions

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,14 @@
 // this is the primary export for general purpose utils
+import fontSizes from './fontSizes';
+import fontWeights from './fontWeights';
 import gridScale from './gridScale';
+import statusColors from './statusColors';
+import uiColors from './uiColors';
 
 export default {
+  fontSizes,
+  fontWeights,
   gridScale,
+  statusColors,
+  uiColors,
 };
