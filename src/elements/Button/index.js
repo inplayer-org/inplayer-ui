4445fa5e1ac0c630fc5a48df812bbb7d2f8fe832
srdjanRakic <srgjan.rakic@gmail.com>
2018-10-15 14:36:08 +0200

    Create button hover modifiers and use util functions

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -1,35 +1,66 @@
 import styled from 'styled-components';
 import { applyStyleModifiers } from 'styled-components-modifiers';
-import { fontColorModifiers } from '../../modifiers';
-import theme from '../../../config/theme';
-
-const {
-  colors: { white, fontcolorSec, lines },
-  font: {
-    weights: { light },
-  },
-} = theme;
+import { fontSizes, uiColors, statusColors, fontWeights } from 'utils';
 
 const modifiers = {
-  ...fontColorModifiers,
+  hoverDanger: ({ theme }) => `
+    &:hover, &:focus {
+      span {
+        color: ${statusColors('danger', theme)};
+      }
+    }
+  `,
+  hoverInfo: ({ theme }) => `
+    &:hover, &:focus {
+      span {
+        color: ${statusColors('info', theme)};
+      }
+    }
+  `,
+  hoverSuccess: ({ theme }) => `
+    &:hover, &:focus {
+      span {
+        color: ${statusColors('success', theme)};
+      }
+    }
+  `,
+  hoverWarning: ({ theme }) => `
+      &:hover, &:focus {
+      span {
+        color: ${statusColors('warning', theme)};
+      }
+    }
+  `,
 };
 
 const Button = styled.button`
-  display: inline-block;
-  background: ${white};
-  color: ${fontcolorSec};
-  font-weight: ${light};
-  border: 1px solid ${lines};
+  display: flex;
+  background: ${uiColors('lines')};
+  align-items: center;
+  color: ${uiColors('fontcolorSec')};
+  font-weight: ${fontWeights('semiBold')};
+  border: 1px solid ${uiColors('lines')};
   padding: 10px 20px 11px;
   border-radius: 3px;
-  font-size: 15px;
-  letter-spacing: 0.015em;
-  line-height: 1;
-  transition: all ease 300ms;
+  height: ${fontSizes('large')};
+  justify-content: center;
+  outline: none;
+  transition: all 200ms ease;
+  width: ${fontSizes('large')};
   cursor: pointer;
   vertical-align: middle;
+
+  &:hover,
+  &:focus {
+    transform: scale(1.2);
+
+    span {
+      color: ${statusColors('default')};
+    }
+  }
+
   ${applyStyleModifiers(modifiers)};
 `;
 
-// @component
+/** @component */
 export default Button;
