d587f9045f2624f25ab5baa455dcae53b8570599
Elena <elena.stojanova@inplayer.com>
2018-10-19 12:14:33 +0200

    Update Radio onChange event

--- src/components/Radio/Readme.md
+++ src/components/Radio/Readme.md
@@ -1,10 +1,23 @@
 RadioInput is a input component for radio. You can use it directly like this:
 
 ```jsx static
+import React from 'react';
 import { Radio } from '@inplayer-org/inplayer-ui';
 
-function Page(props) {
-  return <Radio id="radio1" label="label" onChange={e => console.log(e)} />;
+class Page extends React.Component {
+  state = {
+    checked: false,
+  };
+
+  onRadioChange = checked => {
+    this.setState({
+      checked,
+    });
+  };
+
+  render() {
+    return <Radio id="radio1" label="label" onChange={this.onRadioChange} />;
+  }
 }
 ```
 
--- src/components/Radio/index.js
+++ src/components/Radio/index.js
@@ -6,16 +6,19 @@ import Label from '../../elements/Label';
 type Props = {
   label: Object,
   id: string,
-  name: string,
+  name: ?string,
   value: boolean,
-  onChange: { checked: boolean },
+  onChange: (checked: boolean) => any,
 };
 
-const Radio = ({ label, id, value, ...rest }: Props) => (
-  <div>
-    <RadioInput type="radio" id={id} checked={value} {...rest} />
-    <Label htmlFor={id}>{label}</Label>
-  </div>
-);
+const Radio = ({ label, id, value, onChange, ...rest }: Props) => {
+  const onRadioChange = (e: SyntheticEvent<HTMLInputElement>): any => onChange(e.target.checked);
+  return (
+    <div>
+      <RadioInput type="radio" id={id} checked={value} onChange={onRadioChange} {...rest} />
+      <Label htmlFor={id}>{label}</Label>
+    </div>
+  );
+};
 
 export default Radio;
