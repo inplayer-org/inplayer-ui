864a1e42444d3822b2b8e9626f6e4cb302aa7fe1
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-10-24 16:39:36 +0200

    Implement styles for all CheckboxWrapper states.
    * Change uiColors from regular function to lambda expression.

--- src/components/Checkbox/CheckboxWrapper.js
+++ src/components/Checkbox/CheckboxWrapper.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { applyStyleModifiers } from 'styled-components-modifiers';
 import colors from 'config/colors';
+import uiColors from 'utils/uiColors';
 
 const modifiers = {
   hoverDisabled: () => `
@@ -36,48 +37,68 @@ const CheckboxWrapper = styled.div`
     }
 
     &::after {
-      content: '✔';
+      content: '';
       position: absolute;
-      top: 1px;
-      left: 2px;
-      font-size: 1rem;
-      color: #5ae;
+      top: 5px;
+      left: 4px;
       transition: all ease 300ms;
     }
   }
 
   > input:checked + label {
     &::before {
-      border: 1px solid ${colors.accentBlue};
-    }
-
-    &:hover::before {
-      border: 1px solid ${colors.gray};
-      background: ${colors.accentBlueFade};
+      border: 1px solid ${uiColors('primary.main')};
     }
 
     &::after {
+      border: 2px solid ${uiColors('primary.main')};
+      border-top: none;
+      border-right: none;
+      width: 0.5rem;
+      height: 0.25rem;
       opacity: 1;
-      transform: scale(1);
+      transform: scale(1) rotate(-45deg);
     }
   }
 
   > input:not(:checked) + label {
-    &:hover::before {
-      border: 1px solid ${colors.accentBlue};
+    &::after {
+      border: 2px solid ${colors.white};
+      border-top: none;
+      border-right: none;
+      width: 0.5rem;
+      height: 0.25rem;
+      opacity: 1;
+      transform: scale(0);
+    }
+  }
+
+  > input:checked + label:hover {
+    ::before {
+      border: 1px solid ${colors.gray};
+      background: ${uiColors('primary.light')};
+    }
+
+    ::after {
+      border-color: ${colors.white};
+    }
+  }
+
+  > input:not(:checked) + label:hover {
+    ::before {
+      border: 1px solid ${uiColors('primary.main')};
       background-color: transparent;
     }
 
-    &::after {
-      opacity: 0;
-      transform: scale(0);
+    ::after {
+      transform: scale(1) rotate(-45deg);
     }
   }
 
   > input:checked:focus + label,
   input:not(:checked):focus + label {
     &::before {
-      border: 1px dotted ${colors.accentBlueFade};
+      border: 1px dotted ${uiColors('primary.light')};
     }
   }
 
@@ -88,7 +109,7 @@ const CheckboxWrapper = styled.div`
     }
 
     &::after {
-      color: ${colors.gray};
+      border-color: ${colors.gray};
       background: transparent;
     }
   }
