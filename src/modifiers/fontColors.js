b070b8b26c19a7c98682f4ae8c706b1f142c042e
srdjanRakic <srgjan.rakic@gmail.com>
2018-10-25 14:29:16 +0200

    Generate snapshots

--- src/modifiers/fontColors.js
+++ src/modifiers/fontColors.js
@@ -2,10 +2,13 @@ import colors from 'config/colors';
 import { uiColors } from '../utils';
 
 const fontColorModifiers = {
-  text: ({ theme }) => `
+  textMain: ({ theme }) => `
     color: ${uiColors('text.main', theme)};
   `,
   textLight: ({ theme }) => `
+    color: ${uiColors('text.light', theme)};
+  `,
+  textBold: ({ theme }) => `
     color: ${uiColors('text.light', theme)};
   `,
   blue: () => `
@@ -13,6 +16,9 @@ const fontColorModifiers = {
   `,
   red: () => `
     color: ${colors.red};
+  `,
+  green: () => `
+    color: ${colors.green};
   `,
   disabled: ({ theme }) => `
     color: ${uiColors('text.disabled', theme)};
