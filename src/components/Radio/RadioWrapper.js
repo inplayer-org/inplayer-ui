96396339b7c137144e9e9852aeaa2d62f58cf9b0
Elena <elena.stojanova@inplayer.com>
2018-10-25 14:06:24 +0200

    Added fontSizes modifier

--- src/components/Radio/RadioWrapper.js
+++ src/components/Radio/RadioWrapper.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { uiColors, fontSizes } from 'utils';
+import { uiColors } from 'utils';
 import { applyStyleModifiers } from 'styled-components-modifiers';
 import colors from 'config/colors';
 
