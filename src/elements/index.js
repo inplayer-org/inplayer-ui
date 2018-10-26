7ea7fd135054cb40f942a28d01b52036746137fd
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-10-26 13:55:11 +0200

    feature: Add Typography component.
    * Add snapshot tests for Typography component.

--- src/elements/index.js
+++ src/elements/index.js
@@ -5,3 +5,4 @@ export { default as TextField } from './TextField';
 export { default as TextArea } from './TextArea';
 export { default as Switcher } from './Switcher';
 export { default as Notification } from './Notification';
+export { default as Typography } from './Typography';
