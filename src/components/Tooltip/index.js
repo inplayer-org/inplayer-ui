7aeaf59414aa5479383d19dc6a4c48d6dbe1c603
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-10-31 15:59:44 +0100

    improve: Add optional color prop to Tooltip.

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -6,14 +6,20 @@ type Props = {
   placement: string,
   dataTip: string,
   children: Node,
+  /** Defaults to theme secondary color if no value is passed */
+  color?: string,
 };
 
-const Tooltip = ({ placement, dataTip, children }: Props) => (
-  <TooltipWrapper>
+const Tooltip = ({ placement, dataTip, children, color }: Props) => (
+  <TooltipWrapper color={color}>
     <span tooltip={dataTip} flow={placement}>
       {children}
     </span>
   </TooltipWrapper>
 );
+
+Tooltip.defaultProps = {
+  color: null,
+};
 
 export default Tooltip;
