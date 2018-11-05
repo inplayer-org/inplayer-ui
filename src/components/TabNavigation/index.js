f928c6ac61b1a5694216873e679f1d021f26951d
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-11-05 16:16:34 +0100

    feature: Implement TabNavigation component.

---
+++ src/components/TabNavigation/index.js
@@ -0,0 +1,58 @@
+// @flow
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { ifProp } from 'styled-tools';
+import { uiColors } from 'utils';
+
+const Tab = styled.a`
+  border-bottom: 2px solid transparent;
+  color: ${uiColors('text.light')};
+  cursor: pointer;
+  font-weight: 300;
+  line-height: 2rem;
+  margin-right: 1em;
+  padding: 0 4px;
+  text-decoration: none;
+  transition: color 0.3s ease;
+  ${ifProp(
+    'active',
+    css`
+      border-color: ${uiColors('primary.main')};
+      color: ${uiColors('text.main')};
+    `
+  )};
+
+  &:hover {
+    color: ${uiColors('text.main')};
+  }
+`;
+
+const TabContainer = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+`;
+
+type NavigationTab = {
+  title: string,
+};
+
+type Props = {
+  tabs: Array<NavigationTab>,
+  onTabClick: (index: number) => any,
+  selectedTabIndex: number,
+};
+
+export type TabNavigationProps = Props;
+
+const TabNavigation = ({ tabs, onTabClick, selectedTabIndex }: Props) => (
+  <TabContainer>
+    {tabs.map((tab, index) => (
+      <Tab key={tab.title} onClick={() => onTabClick(index)} active={selectedTabIndex === index}>
+        {tab.title}
+      </Tab>
+    ))}
+  </TabContainer>
+);
+
+export default TabNavigation;
