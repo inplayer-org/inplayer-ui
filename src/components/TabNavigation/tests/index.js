efa1dfec6ce2dedb57a4af15e60d2467b8aac7e1
Andrej Naumovski <a.naumovski996@yahoo.com>
2018-11-05 16:29:03 +0100

    change: Add props to TabNavigation tests.

--- src/components/TabNavigation/tests/index.js
+++ src/components/TabNavigation/tests/index.js
@@ -2,4 +2,12 @@ import { generateSnapshots } from 'helpers';
 
 import TabNavigation from '../index';
 
-generateSnapshots(TabNavigation);
+generateSnapshots(TabNavigation, {
+  tabs: [
+    {
+      title: 'Hello',
+    },
+  ],
+  onTabClick: console.log,
+  selectedTabIndex: 0,
+});
