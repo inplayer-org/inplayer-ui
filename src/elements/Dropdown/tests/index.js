187309697f9f1c31d9951f7d30ede2f9e1923ec6
srdjanRakic <srgjan.rakic@gmail.com>
2018-11-06 16:18:18 +0100

    Update snapshots

--- src/elements/Dropdown/tests/index.js
+++ src/elements/Dropdown/tests/index.js
@@ -2,4 +2,15 @@ import { generateSnapshots } from 'helpers';
 
 import Dropdown from '../index';
 
-generateSnapshots(Dropdown);
+generateSnapshots(Dropdown, {
+  value: 'loremIpsum',
+  options: [
+    {
+      value: 'loremIpsum',
+      displayName: 'Lorem Ipsum',
+    },
+  ],
+  onChange: value => {
+    console.log(value);
+  },
+});
