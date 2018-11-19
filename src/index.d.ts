1fcf7dfdd258d340975e754d5ff5f9603bdc6107
srdjanRakic <srgjan.rakic@gmail.com>
2018-11-19 10:26:07 +0100

    Remove comments in index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -439,7 +439,6 @@ export interface TypographyProps {
 
 export declare const Typography: FunctionComponent<TypographyProps>;
 
-// @flow
 export interface Theme {
   palette: {
     primary: {
@@ -502,37 +501,29 @@ declare export const ThemeWrapper: FunctionComponent<ThemeWrapperProps>;
 
 interface IColors {
   white: '#fff';
-  // dark blue colors
-  blue: '#286090'; // performance
-  darkBlue: '#253a66'; // performanceFilter
-  mediumBlue: '#010185'; // darkBlueFilterLight,
-  // used as background for code snippets
-  midnightBlue: '#272B38'; // coderBlue
-  // basic grays
-  gray: '#d7dde5'; // lines
-  lightGray: '#F6F8FB'; // background
-  darkGray: '#b9c1cb'; // icons
-  // font colors
-  fontDarkGray: '#44494e'; // fontcolorPrim
-  fontGray: '#a0a3aa'; // fontcolorSec
-  fontLightGray: '#c8ccd4'; // fontcolorDis
-  // primary accent colors
+  blue: '#286090';
+  darkBlue: '#253a66';
+  mediumBlue: '#010185';
+  midnightBlue: '#272B38';
+  gray: '#d7dde5';
+  lightGray: '#F6F8FB';
+  darkGray: '#b9c1cb';
+  fontDarkGray: '#44494e';
+  fontGray: '#a0a3aa';
+  fontLightGray: '#c8ccd4';
   lightSkyBlue: '#97CCF6';
-  skyBlue: '#07AAE6'; // accentBlue
-  deepSkyBlue: '#0a8cd2'; // accentBlueFade
-  navy: '#2A4D8E'; // performanceDark
+  skyBlue: '#07AAE6';
+  deepSkyBlue: '#0a8cd2';
+  navy: '#2A4D8E';
   accentPink: '#fcc8be';
-  // secondary accent colors
   lemon: '#fcf3c0';
   mint: '#c8e8de';
-  // extra light background color only for tables
-  neutralGray: '#f6f8fb'; // tableBg
-  // used for info boxes and warnings
-  red: '#f26f59'; // danger
+  neutralGray: '#f6f8fb';
+  red: '#f26f59';
   paleRed: '#f9ece8';
   yellow: '#f4ca19';
   paleYellow: '#fffcee';
-  green: '#5dc6a3'; // success
+  green: '#5dc6a3';
   paleGreen: '#e4f9f2';
   black: '#000';
 }
