0005153aebc340d86ecb9322d8a9376a3cd8087a
Andrej Naumovski <a.naumovski996@yahoo.com>
2019-03-20 16:18:48 +0100

    feat: Enable loading of icon font.

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,71 @@ const pkg = require('./package.json');
 module.exports = {
   entry: ['./src/index'],
   module: {
-    rules: [{ test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ }],
+    rules: [
+      { test: /\.js?$/, loader: 'babel-loader', exclude: /node_modules/ },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000,
+              mimetype: 'application/font-woff',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000,
+              mimetype: 'application/octet-stream',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000,
+              mimetype: 'image/svg+xml',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000,
+              mimetype: 'application/vnd.ms-fontobject',
+              name: 'fonts/[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
   resolve: {
     modules: [path.resolve('./'), path.resolve('./node_modules')],
     extensions: ['.js'],
     alias: {
+      assets: path.resolve(__dirname, './src/assets'),
       components: path.resolve(__dirname, './src/components'),
       blocks: path.resolve(__dirname, './src/blocks'),
       elements: path.resolve(__dirname, './src/elements'),
