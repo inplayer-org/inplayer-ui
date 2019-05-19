2be91a00c5f4b99f19d8729fe3a79a73a4c3eb78
srdjanRakic <srgjan.rakic@gmail.com>
2019-05-19 19:38:01 +0200

    Create Tooltip Wrapper component

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,32 +1,139 @@
 // @flow
-import React, { type Node } from 'react';
-import TooltipWrapper from './TooltipWrapper';
+import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 
-type TooltipVariant = 'up' | 'down' | 'left' | 'right';
+import Arrow from './Arrow';
+import TooltipWrapper from './Tooltip';
+import Bubble from './Bubble';
+import type { FadeEasing } from './Tooltip';
+
+export type Placement = 'left' | 'right' | 'top' | 'bottom';
 
 type Props = {
-  placement: TooltipVariant,
-  dataTip: string,
-  children: Node,
-  /** A className can be passed down for further styling or extending with CSS-in-JS */
-  className?: string,
-  /** Defaults to theme secondary color if no value is passed */
+  arrowWidth?: number,
+  background?: string,
+  border?: string,
+  children?: Node,
   color?: string,
-  style?: Object,
+  content: Node,
+  className?: string,
+  style?: string,
+  fadeDuration?: number,
+  fadeEasing?: FadeEasing,
+  fixed?: boolean,
+  fontFamily?: string,
+  fontSize?: string,
+  offset?: number,
+  padding?: number,
+  placement?: Placement,
+  radius?: number,
+  zIndex?: number,
 };
 
-const Tooltip = ({ placement, dataTip, children, color, className, style }: Props) => (
-  <TooltipWrapper color={color} className={className} style={style}>
-    <span tooltip={dataTip} flow={placement}>
-      {children}
-    </span>
-  </TooltipWrapper>
-);
+type State = {
+  isOpen: boolean,
+};
+
+const Container = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+class Tooltip extends PureComponent<Props, State> {
+  state = {
+    isOpen: false,
+  };
+
+  handleMouseEnter = () => {
+    this.setState({ isOpen: true });
+  };
+
+  handleMouseLeave = () => {
+    this.setState({ isOpen: false });
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const {
+      arrowWidth,
+      background,
+      border,
+      children,
+      color,
+      content,
+      style,
+      className,
+      fadeDuration,
+      fadeEasing,
+      fixed,
+      fontFamily,
+      fontSize,
+      offset,
+      padding,
+      placement,
+      radius,
+      zIndex,
+    } = this.props;
+
+    const tooltipElement = (
+      <TooltipWrapper
+        isOpen={!children || fixed ? true : isOpen}
+        placement={placement}
+        offset={offset + arrowWidth}
+        zIndex={zIndex}
+        fadeEasing={fadeEasing}
+        fadeDuration={fadeDuration}
+      >
+        <Bubble
+          background={background}
+          border={border}
+          color={color}
+          radius={radius}
+          fontFamily={fontFamily}
+          fontSize={fontSize}
+          padding={padding}
+        >
+          <Arrow width={arrowWidth} background={background} border={border} placement={placement} />
+          {content}
+        </Bubble>
+      </TooltipWrapper>
+    );
+    return children ? (
+      <Container
+        style={style}
+        className={className}
+        onMouseEnter={!fixed ? this.handleMouseEnter : undefined}
+        onMouseLeave={!fixed ? this.handleMouseLeave : undefined}
+      >
+        {children}
+        {tooltipElement}
+      </Container>
+    ) : (
+      <Container className={className} style={style}>
+        {tooltipElement}
+      </Container>
+    );
+  }
+}
 
 Tooltip.defaultProps = {
-  className: '',
-  color: null,
-  style: {},
+  arrowWidth: 8,
+  background: '',
+  border: '',
+  children: null,
+  color: '',
+  fadeDuration: 0,
+  fadeEasing: 'linear',
+  fixed: false,
+  fontFamily: 'inherit',
+  fontSize: 'inherit',
+  offset: 0,
+  padding: 1,
+  placement: 'top',
+  radius: 0,
+  zIndex: 1,
+  style: null,
+  className: null,
 };
 
 export default Tooltip;
