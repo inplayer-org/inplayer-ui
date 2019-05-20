365eb6239ac5ebd1d6416df35465352c0acdd39b
srdjanRakic <srgjan.rakic@gmail.com>
2019-05-20 15:11:57 +0200

    Use FunctionComponent instead of Class

--- src/elements/StepWizard/index.js
+++ src/elements/StepWizard/index.js
@@ -1,157 +1,76 @@
 // @flow
-import React, { PureComponent } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import Nav from './Nav';
 import Step from './Step';
 
 type TransitionVariant = 'fadeInLeft' | 'fadeInRight' | 'fadeOutLeft' | 'fadeOutRight';
 
 type Props = {
-  children?: React.ReactNode,
-  className?: string,
-  initialStep?: number,
-  nav?: React.ReacNode,
-  isHashEnabled?: boolean,
-  onStepChange?: Function,
-  transition?: TransitionVariant,
-};
-
-type State = {
+  children: React.ReactNode,
   activeStep: number,
+  className?: string,
+  style?: Object,
+  onStepChange?: (activeStep: number) => void,
+  transition?: TransitionVariant,
 };
 
 const WizardContainer = styled.div`
   position: relative;
 `;
 
-export default class StepWizard extends PureComponent<Props, State> {
-  state = {
-    activeStep: 0,
-  };
+const StepWizard = ({
+  children,
+  className,
+  style,
+  activeStep,
+  onStepChange,
+  transition,
+}: Props) => {
+  const isInvalidStep = currentStep => currentStep < 0 || currentStep > children.length;
 
-  isInvalidStep = next => {
-    const { children } = this.props;
-
-    return next < 0 || next >= children.length;
-  };
-
-  setActiveStep = nextStep => {
-    const { activeStep } = this.state;
-    if (activeStep === nextStep) return;
-    if (this.isInvalidStep(nextStep)) {
-      console.error(`${nextStep + 1} is an invalid step`);
+  const setActiveStep = currentStep => {
+    if (activeStep === currentStep) return;
+    if (isInvalidStep(currentStep)) {
+      console.error(`${currentStep} is an invalid step`);
       return;
     }
 
-    this.setState(
-      {
-        activeStep: nextStep,
-      },
-      () => {
-        // Step change callback
-        this.onStepChange({
-          previousStep: activeStep + 1,
-          activeStep: nextStep + 1,
-        });
-      }
-    );
-  };
-
-  onStepChange = stats => {
-    const { onStepChange, isHashEnabled } = this.props;
-    const { activeStep } = this.state;
-
     // User callback
-    onStepChange(stats);
-
-    // Update hash if prop set
-    if (isHashEnabled) this.updateHash(activeStep);
-  };
-
-  /** Go to first step */
-  firstStep = () => this.goToStep(1);
-
-  /** Go to last step */
-  lastStep = () => {
-    const { children } = this.props;
-    this.goToStep(children.length);
-  };
-
-  /** Next Step */
-  lastStep = () => {
-    const { activeStep } = this.state;
-    this.setActiveStep(activeStep + 1);
-  };
-
-  /** Previous Step */
-  previousStep = () => {
-    const { activeStep } = this.state;
-    this.setActiveStep(activeStep - 1);
+    onStepChange(currentStep);
   };
 
   /** Go to step index */
-  goToStep = step => this.setActiveStep(step - 1);
+  const goToStep = step => setActiveStep(step);
 
-  // updateHash = activeStep => {
-  //   const { hashKeys } = this.state;
-  //   window.location.hash = hashKeys[activeStep];
-  // };
+  const childrenWithProps = React.Children.map(children, (child, i) => {
+    // child has 0 starting index
+    const isStepActive = i + 1 === activeStep;
 
-  // Allows for using HTML elements as a step
-  isReactComponent = ({ type }) => {
-    console.error(typeof type === 'function' || typeof type === 'object');
-    return typeof type === 'function' || typeof type === 'object';
-  };
+    if (isStepActive) {
+      return (
+        <Step isActive={isStepActive} transition={transition}>
+          {child}
+        </Step>
+      );
+    }
 
-  /** Render */
-  render() {
-    const { activeStep } = this.state;
-    const { children, transition, initialStep } = this.props;
+    return null;
+  });
 
-    const props = {
-      currentStep: activeStep + 1,
-      totalSteps: children.length,
-      /** Functions */
-      nextStep: this.nextStep,
-      previousStep: this.previousStep,
-      goToStep: this.goToStep,
-      firstStep: this.firstStep,
-      lastStep: this.lastStep,
-    };
-
-    const childrenWithProps = React.Children.map(children, (child, i) => {
-      const isStepActive = i === initialStep;
-
-      console.error(child);
-
-      if (isStepActive) {
-        return (
-          <Step isActive={isStepActive} transition={transition}>
-            {this.isReactComponent(child) ? React.cloneElement(child, props) : child}
-          </Step>
-        );
-      }
-
-      return null;
-    });
-
-    const { nav, className } = this.props;
-
-    return (
-      <div className={className}>
-        {nav && React.cloneElement(nav, props)}
-        <WizardContainer>{childrenWithProps}</WizardContainer>
-      </div>
-    );
-  }
-}
+  return (
+    <div className={className} style={style}>
+      <Nav activeStep={activeStep} totalSteps={children.length} goToStep={goToStep} />
+      <WizardContainer>{childrenWithProps}</WizardContainer>
+    </div>
+  );
+};
 
 StepWizard.defaultProps = {
-  children: [],
   className: null,
-  initialStep: 1,
-  instance: () => {},
-  isHashEnabled: false,
-  nav: null,
-  transition: null,
+  style: {},
+  transition: 'fadeInRight',
   onStepChange: () => {},
 };
+
+export default StepWizard;
