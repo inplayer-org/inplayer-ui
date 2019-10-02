2fafe1054d3e2a8b81c001971198ebce871822bd
jasmina <jasmina5j@gmail.com>
2019-10-02 11:16:07 +0200

    Remove console log

--- src/elements/DayPicker/index.js
+++ src/elements/DayPicker/index.js
@@ -26,22 +26,18 @@ const DayPicker = ({
   style,
   className,
   numberOfMonths,
-}: Props) => {
-  console.log(typeof date, 'TYPE Of UIIIIIII');
-  console.log(typeof date === 'string' ? moment(date, 'MM-DD-YYYY') : date, 'DATE');
-  return (
-    <DayPickerWrapper style={style} className={className}>
-      <SingleDatePicker
-        isOutsideRange={isOutsideRange}
-        onDateChange={onDateChange}
-        onFocusChange={onFocusChange}
-        focused={focused}
-        date={typeof date === 'string' ? moment(date, 'MM-DD-YYYY') : date}
-        numberOfMonths={numberOfMonths}
-      />
-    </DayPickerWrapper>
-  );
-};
+}: Props) => (
+  <DayPickerWrapper style={style} className={className}>
+    <SingleDatePicker
+      isOutsideRange={isOutsideRange}
+      onDateChange={onDateChange}
+      onFocusChange={onFocusChange}
+      focused={focused}
+      date={typeof date === 'string' ? moment(date, 'MM-DD-YYYY') : date}
+      numberOfMonths={numberOfMonths}
+    />
+  </DayPickerWrapper>
+);
 
 DayPicker.defaultProps = {
   isOutsideRange: () => false,
