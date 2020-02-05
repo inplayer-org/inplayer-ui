57b8d10a9f07b2b919963b604c3f04cdc7f08cde
srdjanRakic <srgjan.rakic@gmail.com>
2020-02-05 13:39:06 +0100

    Add react and react-dom as externals in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -101,4 +101,18 @@ module.exports = {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new CopyWebpackPlugin([{ from: './src/index.d.ts', to: './index.d.ts' }]),
   ],
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
 };
