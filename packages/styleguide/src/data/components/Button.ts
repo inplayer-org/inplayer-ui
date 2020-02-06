2e23d1522b4ebccc62244254bf71730ae5d09499
Aleksandra Nikova <aleksandra.nikova@inplayer.com>
2020-02-06 16:10:03 +0100

    Changed code in components

--- packages/styleguide/src/data/components/Button.ts
+++ packages/styleguide/src/data/components/Button.ts
@@ -1,29 +1,32 @@
 import { ComponentData } from '../data';
 
+const usage = `
+  import { Button } from '@inplayer-org/inplayer-ui';
+    
+  function Page(props) {
+    return <Button>Buy now</Button>;
+  }
+`;
+
+const demoCode = `<div>
+  <Button size="xs">Button XS</Button> <br />
+  <Button size="sm">Button SM</Button> <br />
+  <Button size="md">Button MD</Button> <br />
+  <Button size="lg">Button LG</Button> <br />
+  <Button fullWidth>Button Full Width</Button> <br />
+</div>
+`;
+
 const Button: ComponentData = {
-    description: ``,
-    path: `src/components/Button/index.js`,
-    usage: `
-    import { Button } from '@inplayer-org/inplayer-ui';
-  
-    function Page(props) {
-      return <Button>Buy now</Button>;
+  description: ``,
+  path: `src/components/Button/index.js`,
+  usage,
+  examples: [
+    {
+      title: 'Demo',
+      code: demoCode,
     }
-   `,
-    examples: [
-        {
-            title: "Demo",
-            code: `
-        <div>
-          <Button size="xs">Button XS</Button> <br />
-          <Button size="sm">Button SM</Button> <br />
-          <Button size="md">Button MD</Button> <br />
-          <Button size="lg">Button LG</Button> <br />
-          <Button fullWidth>Button Full Width</Button> <br />
-        </div>
-        `
-        }
-    ]
+  ],
 }
 
 export default Button;
--- packages/styleguide/src/data/components/Modal.ts
+++ packages/styleguide/src/data/components/Modal.ts
@@ -1,112 +1,115 @@
 import { ComponentData } from '../data';
 
-const Modal: ComponentData = {
-    description: ``,
-    path: `src/components/Modal/index.js`,
-    usage: `
-    import { Modal } from 'inplayer-ui';
+const usage = `
+  import { Modal } from 'inplayer-ui';
 
-    class App extends React.Component {
-      this.state = {
+  class App extends React.Component {
+    this.state = {
+      isModalOpen: false,
+    };
+
+    closeModal = () => {
+      this.setState({
         isModalOpen: false,
-      };
-    
-      closeModal = () => {
-        this.setState({
-          isModalOpen: false,
-        });
-      };
-    
-      openModal = () => {
-        this.setState({
-          isModalOpen: true,
-        });
-      };
-    
-      render() {
-        return (
-          <div>
-            <button onClick={this.openModal}>
-              Open modal
-            </button>
-    
-            <Modal
-              title="Lorem Ipsum"
-              isModalOpen={this.state.isModalOpen}
-              closeModal={this.closeModal}
-            >
-               <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-                pretium nec nisi pulvinar lacinia. Sed dapibus id dui vel varius.
-                Duis erat ligula, posuere nec massa nec, ultricies bibendum velit.
-                Nam vehicula lectus a convallis varius. Duis ullamcorper felis ac
-                sem pulvinar pretium pellentesque id urna. Aliquam erat volutpat.
-                Pellentesque sagittis congue lorem, at ultricies mauris lobortis a.
-                Nam dignissim lectus eget tellus eleifend dictum.
-              </p>
-            </Modal>
-          </div>
-        );
-      }
+      });
+    };
+
+    openModal = () => {
+      this.setState({
+        isModalOpen: true,
+      });
+    };
+
+    render() {
+      return (
+        <div>
+          <button onClick={this.openModal}>
+            Open modal
+          </button>
+
+          <Modal
+            title="Lorem Ipsum"
+            isModalOpen={this.state.isModalOpen}
+            closeModal={this.closeModal}
+          >
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              pretium nec nisi pulvinar lacinia. Sed dapibus id dui vel varius.
+              Duis erat ligula, posuere nec massa nec, ultricies bibendum velit.
+              Nam vehicula lectus a convallis varius. Duis ullamcorper felis ac
+              sem pulvinar pretium pellentesque id urna. Aliquam erat volutpat.
+              Pellentesque sagittis congue lorem, at ultricies mauris lobortis a.
+              Nam dignissim lectus eget tellus eleifend dictum.
+            </p>
+          </Modal>
+        </div>
+      );
     }
-    `,
-    examples: [
-        {
-            title: "Demo",
-            code: `
-            class App extends React.Component {
-                // set initial state
-                constructor() {
-                  super();
-              
-                  this.state = {
-                    isModalOpen: false,
-                  };
-              
-                  // bind functions
-                  this.closeModal = this.closeModal.bind(this);
-                  this.openModal = this.openModal.bind(this);
-                }
-              
-                closeModal() {
-                  this.setState({
-                    isModalOpen: false,
-                  });
-                }
-              
-                openModal() {
-                  this.setState({
-                    isModalOpen: true,
-                  });
-                }
-              
-                render() {
-                  return (
-                    <div>
-                      <Button onClick={this.openModal}>Open modal</Button>
-              
-                      <Modal
-                        title="Lorem Ipsum"
-                        isModalOpen={this.state.isModalOpen}
-                        closeModal={this.closeModal}
-                      >
-                        <p>
-                          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
-                          pretium nec nisi pulvinar lacinia. Sed dapibus id dui vel varius.
-                          Duis erat ligula, posuere nec massa nec, ultricies bibendum velit.
-                          Nam vehicula lectus a convallis varius. Duis ullamcorper felis ac
-                          sem pulvinar pretium pellentesque id urna. Aliquam erat volutpat.
-                          Pellentesque sagittis congue lorem, at ultricies mauris lobortis a.
-                          Nam dignissim lectus eget tellus eleifend dictum.
-                        </p>
-                      </Modal>
-                    </div>
-                  );
-                }
-              }
-            `
-        }
-    ]
+  }
+`;
+
+const demoCode = `class App extends React.Component {
+  // set initial state
+  constructor() {
+    super();
+
+    this.state = {
+      isModalOpen: false,
+    };
+
+    // bind functions
+    this.closeModal = this.closeModal.bind(this);
+    this.openModal = this.openModal.bind(this);
+  }
+
+  closeModal() {
+    this.setState({
+      isModalOpen: false,
+    });
+  }
+
+  openModal() {
+    this.setState({
+      isModalOpen: true,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Button onClick={this.openModal}>Open modal</Button>
+
+        <Modal
+          title="Lorem Ipsum"
+          isModalOpen={this.state.isModalOpen}
+          closeModal={this.closeModal}
+        >
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            pretium nec nisi pulvinar lacinia. Sed dapibus id dui vel varius.
+            Duis erat ligula, posuere nec massa nec, ultricies bibendum velit.
+            Nam vehicula lectus a convallis varius. Duis ullamcorper felis ac
+            sem pulvinar pretium pellentesque id urna. Aliquam erat volutpat.
+            Pellentesque sagittis congue lorem, at ultricies mauris lobortis a.
+            Nam dignissim lectus eget tellus eleifend dictum.
+          </p>
+        </Modal>
+      </div>
+    );
+  }
+}
+`;
+
+const Modal: ComponentData = {
+  description: ``,
+  path: `src/components/Modal/index.js`,
+  usage,
+  examples: [
+    {
+      title: 'Demo',
+      code: demoCode,
+    }
+  ],
 }
 
 export default Modal;
