1d61214d563cdbac77fb00c83fdf29ca6de6b4f3
Kristina Taneva <kristina.taneva@inplayer.com>
2020-02-11 15:58:04 +0100

    Removed unnecessary function

--- packages/styleguide/src/utils/generateComponentData.ts
+++ packages/styleguide/src/utils/generateComponentData.ts
@@ -2,11 +2,6 @@ import data from '../data/data';
 
 type UiComponentType = keyof typeof data;
 
-export const getPropsAndMethods = (componentType: UiComponentType) => {
-    const component = data[componentType];
-    return component ? component.propsAndMethods : ``;
-}
-
 export const getComponent = (componentType: UiComponentType) => {
     return data[componentType] || {};
 }
