675de889bf6c5f7195499de99e470f31a01af26b
srdjanRakic <srgjan.rakic@gmail.com>
2020-02-26 13:28:46 +0100

    feat: export all preview templates

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export * from './blocks';
 export * from './components';
 export * from './elements';
+export * from './previews';
 export { default as ThemeWrapper } from 'theme/ThemeWrapper';
 export { default as Colors } from 'config/colors';
