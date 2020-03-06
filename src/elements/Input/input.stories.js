f65e81db7696b137ec58e6f9a5ac559a362adea6
lalkovski <ilija.lalkovski@inplayer.com>
2020-03-06 11:35:01 +0100

    Add description in component story

--- src/elements/Input/input.stories.js
+++ src/elements/Input/input.stories.js
@@ -1,4 +1,7 @@
+// @flow
 import React from 'react';
+
+// components
 import Input from './index';
 import InPlayerIcon from '../InPlayerIcon/index';
 
@@ -8,32 +11,58 @@ export default {
 };
 
 export const withText = () => (
-  <Input type="text" placeholder="text" onChange={console.log} style={{ marginTop: '1rem' }} />
+  <>
+    <div>Text input is a input component for text. You can use it directly like this:</div>
+    <Input type="text" placeholder="text" onChange={console.log} style={{ marginTop: '1rem' }} />
+  </>
 );
 
 export const searchField = () => (
-  <Input type="search" placeholder="Search" onChange={console.log} style={{ marginTop: '1rem' }} />
+  <>
+    <div>
+      Text input is a input component for text. You can use it for a search component like this:
+    </div>
+    <Input
+      type="search"
+      placeholder="Search"
+      onChange={console.log}
+      style={{ marginTop: '1rem' }}
+    />
+  </>
 );
 
 export const withIcon = () => (
-  <Input type="text" placeholder="Input with icon" style={{ marginTop: '1rem' }} iconName="lock" />
+  <>
+    <div>Text input is a input component for text. You can use it with an icon like this:</div>
+    <Input
+      type="text"
+      placeholder="Input with icon"
+      style={{ marginTop: '1rem' }}
+      iconName="lock"
+    />
+  </>
 );
 
 export const withCustomIcon = () => (
-  <Input
-    type="text"
-    placeholder="Input with custom icon"
-    style={{ marginTop: '1rem' }}
-    renderIcon={() => (
-      <div style={{ marginLeft: '5px', top: '30px', position: 'relative' }}>
-        <InPlayerIcon name="download" modifiers={['statusColorSuccess']} />
-      </div>
-    )}
-  />
+  <>
+    <div>
+      Text input is a input component for text. You can use it with a custom icon like this:
+    </div>
+    <Input
+      type="text"
+      placeholder="Input with custom icon"
+      renderIcon={() => (
+        <div style={{ marginLeft: '5px', top: '30px', position: 'relative' }}>
+          <InPlayerIcon name="download" modifiers={['statusColorSuccess']} />
+        </div>
+      )}
+    />
+  </>
 );
 
 export const withSizes = () => (
   <>
+    <div>Text input is a input component for text. You can use it in these different sizes:</div>
     <Input size="xs" type="text" placeholder="Extra Small Input" style={{ marginTop: '1rem' }} />
     <Input size="sm" type="text" placeholder="Small Input" style={{ marginTop: '1rem' }} />
     <Input size="md" type="text" placeholder="Medium Input" style={{ marginTop: '1rem' }} />
