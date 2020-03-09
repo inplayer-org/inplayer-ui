3a0f766dd4ba96250f1caa0b9804333c02c36c12
mustran <ademimusli000@gmail.com>
2020-03-09 10:47:47 +0100

    Add Title with Typography component

--- src/elements/Loader/loader.stories.js
+++ src/elements/Loader/loader.stories.js
@@ -8,14 +8,14 @@ export default {
 
 export const DefaultLoader = () => (
   <>
-    <h3>Standard loader</h3>
+    <Typography variant="h3">Standard Loader</Typography>
     <Loader />
   </>
 );
 
 export const LoaderWithProps = () => (
   <>
-    <h3>Loader with props</h3>
+    <Typography variant="h3">Loader with props</Typography>
     <Loader height={150} width={150} direction="left" lineWidth={3} />
   </>
 );
