4de6cf6cec5eaeecf89b149e80c2e9d403155b36
mustran <ademimusli000@gmail.com>
2020-03-09 10:05:11 +0100

    Convert the component to functional component

--- src/elements/DatePicker/datepicker.stories.js
+++ src/elements/DatePicker/datepicker.stories.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/destructuring-assignment */
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import DatePicker from './index';
 import moment from '../../../../../.cache/typescript/3.7/node_modules/moment/moment';
@@ -16,72 +16,58 @@ const Container = styled.div`
   width: 90%;
 `;
 
-class DatePickerExample extends React.Component {
-  constructor() {
-    super();
+export const Demo = () => {
+  const initialStartDate = moment().subtract(1, 'month');
+  const initialEndDate = moment();
+  const [startDate, setStartDate] = useState(initialStartDate);
+  const [endDate, setEndDate] = useState(initialEndDate);
+  const [focusedInput, setFocusedInput] = useState(null);
 
-    this.state = {
-      startDate: moment().subtract(1, 'month'),
-      endDate: moment(),
-      focusedInput: null,
-    };
+  // eslint-disable-next-line no-shadow
+  const onDateChange = ({ startDate, endDate }) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+  };
 
-    this.onDateChange = this.onDateChange.bind(this);
-    this.onFocusChange = this.onFocusChange.bind(this);
-  }
+  const onFocusChange = fInput => {
+    setFocusedInput(fInput);
+  };
 
-  onDateChange({ startDate, endDate }) {
-    this.setState({
-      startDate,
-      endDate,
-    });
-  }
-
-  onFocusChange(focusedInput) {
-    this.setState({ focusedInput });
-  }
-
-  render() {
-    return (
+  return (
+    <Container>
+      <Typography variant="h3">Date Picker</Typography>
+      <Typography variant="p" description>
+        DatePicker is a component for picking range for dates. It takes:
+        <br />
+        <b>- startDate</b> and <b>endDate</b> as a prop( momentPropTypes.momentObj or null)
+        <br />- <b>startDateId</b> and <b>endDateId</b>
+        <br />- <b>onDateChange</b> function for setting the state for the dates
+        <br />- <b>displayPresets</b> prop is an array where the user defines which presets to be
+        displayed to the date picker. Available presets are: this week, last week, last 2 weeks,
+        this month, last month, last 6 months, this year, all time. Default option is available as
+        well and if the user adds only default prop to the array, following presets will be
+        displayed: this week, last week, this month, last month, this year,
+        <br />- <b>isOutsideRange</b> a function which specifies the days that are not allowed to be
+        choosed (ex. isOutsideRange={'{'}day => day.isBefore(moment()){'}'}).
+        <br />- <b>customAllTimeDate</b> a timestamp that defines from which date the all time
+        starts in the preset
+        <br />- <b>activePeriod</b> prop is a string where the user defines which presets period to
+        be selected to the date picker. Available presets periods are those present in
+        displayPresets
+      </Typography>
       <DatePicker
-        startDate={this.state.startDate}
+        startDate={startDate}
         startDateId="startDate"
-        endDate={this.state.endDate}
+        endDate={endDate}
         endDateId="endDate"
-        onDateChange={this.onDateChange}
+        onDateChange={onDateChange}
         displayPresets={['default']}
         isOutsideRange={() => false}
-        onFocusChange={this.onFocusChange}
-        focusedInput={this.state.focusedInput}
+        onFocusChange={onFocusChange}
+        focusedInput={focusedInput}
         customAllTimeDate={1514208111}
         activePeriodPreset="last month"
       />
-    );
-  }
-}
-
-export const Demo = () => (
-  <Container>
-    <Typography variant="h3">Date Picker</Typography>
-    <Typography variant="p" description>
-      DatePicker is a component for picking range for dates. It takes:
-      <br />
-      <b>- startDate</b> and <b>endDate</b> as a prop( momentPropTypes.momentObj or null)
-      <br />- <b>startDateId</b> and <b>endDateId</b>
-      <br />- <b>onDateChange</b> function for setting the state for the dates
-      <br />- <b>displayPresets</b> prop is an array where the user defines which presets to be
-      displayed to the date picker. Available presets are: this week, last week, last 2 weeks, this
-      month, last month, last 6 months, this year, all time. Default option is available as well and
-      if the user adds only default prop to the array, following presets will be displayed: this
-      week, last week, this month, last month, this year,
-      <br />- <b>isOutsideRange</b> a function which specifies the days that are not allowed to be
-      choosed (ex. isOutsideRange=
-      {day => day.isBefore(moment())}).
-      <br />- <b>customAllTimeDate</b> a timestamp that defines from which date the all time starts
-      in the preset
-      <br />- <b>activePeriod</b> prop is a string where the user defines which presets period to be
-      selected to the date picker. Available presets periods are those present in displayPresets
-    </Typography>
-    <DatePickerExample />
-  </Container>
-);
+    </Container>
+  );
+};
