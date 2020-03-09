82185d4c10bf0af4ed75813bddc21c7bb1814515
sandrananceva <sandra.nanceva@gmail.com>
2020-03-09 14:22:50 +0100

    added radio

---
+++ src/components/Radio/radio.stories.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Colors, Typography } from 'src/index';
+import Radio from './index';
+
+export default {
+  component: Radio,
+  title: 'Radio',
+};
+
+const Container = styled.div`
+  margin: auto;
+  width: 90%;
+`;
+const BoxContainer = styled.div`
+  border: 1px solid ${Colors.darkGray};
+  width: 90%;
+  padding: 30px;
+`;
+const HeadlineTypography = styled(Typography)`
+  color: ${Colors.black};
+`;
+
+export const Demo = () => (
+  <Container>
+    <HeadlineTypography variant="h3">Radio</HeadlineTypography>
+    <Typography variant="p" description>
+      Radio Input is a input component for radio.
+    </Typography>
+    <BoxContainer>
+      <Radio id="radioUnchecked" label="Check me!" onChange={e => console.log(e)} />
+      <br />
+      <Radio id="radioChecked" label="Checked by default!" checked onChange={e => console.log(e)} />
+      <br />
+      <Radio
+        id="radioDisabledUnchecked"
+        disabled
+        label="Disabled and unchecked!"
+        onChange={e => console.log(e)}
+      />
+      <br />
+      <Radio
+        id="radioDisabledChecked"
+        disabled
+        checked
+        label="Disabled and checked!"
+        onChange={e => console.log(e)}
+      />
+    </BoxContainer>
+  </Container>
+);
