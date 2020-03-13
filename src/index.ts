701c4ec253090d071a5544cfa608f63fc73600dc
srdjanRakic <srgjan.rakic@gmail.com>
2020-03-13 13:52:03 +0100

    chore: move colors to theme folder

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * from './components';
 export * from './elements';
 export * from './previews';
 export { default as ThemeWrapper } from 'theme/ThemeWrapper';
-export { default as Colors } from 'config/colors';
+export { default as Colors } from 'theme/colors';
