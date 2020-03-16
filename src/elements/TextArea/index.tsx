c31e06bf39ef35d7c7bf8585681480e91a5a42fb
sandrananceva <sandra.nanceva@gmail.com>
2020-03-16 13:34:35 +0100

    Refactored TextArea in Typescript

--- src/elements/TextArea/index.tsx
+++ src/elements/TextArea/index.tsx
@@ -1,18 +1,17 @@
 import styled from 'styled-components';
 import { fontWeights, fontSizes } from 'utils';
-
-import { Colors } from 'index';
+import colors from 'theme/colors';
 
 const TextArea = styled.textarea`
   vertical-align: middle;
   box-sizing: border-box;
   overflow: hidden;
   outline: none;
-  color: ${Colors.fontDarkGray};
+  color: ${colors.fontDarkGray};
   transition: all ease 300ms;
-  background: ${Colors.white};
+  background: ${colors.white};
   font-weight: ${fontWeights('light')};
-  border: 1px solid ${Colors.gray};
+  border: 1px solid ${colors.gray};
   border-radius: 3px;
   font-size: ${fontSizes('medium')};
   line-height: 1.4em;
@@ -22,7 +21,7 @@ const TextArea = styled.textarea`
   padding: 0.75rem 1.125rem;
 
   &:focus {
-    border-bottom: 1px solid ${Colors.skyBlue};
+    border-bottom: 1px solid ${colors.skyBlue};
   }
 `;
 export default TextArea;
