f99c0ae8a3f2eb3406796be12e333a8f1142c966
mustran <ademimusli000@gmail.com>
2020-03-17 15:36:30 +0100

    Change folder structure

---
+++ src/elements/DayPicker/DayPicker.tsx
@@ -0,0 +1,53 @@
+// @flow
+import React from 'react';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import moment, { Moment } from 'moment';
+
+import DayPickerWrapper from './DayPickerWrapper';
+
+type Props = {
+  isOutsideRange?: (day: number) => any;
+  onDateChange: (date: Moment) => any;
+  onFocusChange: (focused: any) => any;
+  focused: boolean;
+  date: Moment | string;
+  style?: Object;
+  className?: string;
+  numberOfMonths?: number;
+  disabled?: boolean;
+};
+
+const DayPicker = ({
+  isOutsideRange,
+  onDateChange,
+  onFocusChange,
+  focused,
+  date,
+  style,
+  className,
+  numberOfMonths,
+  disabled,
+}: Props) => (
+  <DayPickerWrapper style={style} className={className}>
+    <SingleDatePicker
+      isOutsideRange={isOutsideRange}
+      onDateChange={onDateChange}
+      onFocusChange={onFocusChange}
+      focused={focused}
+      date={typeof date === 'string' ? moment(date) : date}
+      numberOfMonths={numberOfMonths}
+      disabled={disabled}
+    />
+  </DayPickerWrapper>
+);
+
+DayPicker.defaultProps = {
+  isOutsideRange: () => false,
+  style: {},
+  className: '',
+  numberOfMonths: 1,
+  disabled: false,
+};
+
+export default DayPicker;
