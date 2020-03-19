e10c76c95a7d93a8b1793ff380f7a2a0d8088bea
lalkovski <ilija.lalkovski@inplayer.com>
2020-03-19 16:36:56 +0100

    Fix Typescript errors in Grid component

--- src/blocks/Grid/Container.tsx
+++ src/blocks/Grid/Container.tsx
@@ -4,26 +4,23 @@ interface Props {
   height: string;
   flow: string;
 }
-
-const autoRows = ({ minRowHeight = '20px' }) => `minmax(${minRowHeight}, auto)`;
-
+const autoRows = ({ minRowHeight = '20px' }: Record<string, any>) =>
+  `minmax(${minRowHeight}, auto)`;
 const frGetter = (value: number) => (typeof value === 'number' ? `repeat(${value}, 1fr)` : value);
-
 const formatAreas = (areas: any) => areas.map((area: any) => `"${area}"`).join(' ');
-
 const GridContainer = styled.div<Props>`
   display: grid;
   height: ${({ height = 'auto' }) => height};
   grid-auto-flow: ${({ flow = 'row' }) => flow};
   grid-auto-rows: ${autoRows};
   ${(rows: any) => rows && `grid-template-rows: ${frGetter(rows)}`};
-  grid-template-columns: ${({ columns = 12 }) => frGetter(columns)};
-  grid-gap: ${({ gap = '8px' }) => gap};
-  ${(columnGap: string) => columnGap && `column-gap: ${columnGap}`};
-  ${(rowGap: string) => rowGap && `row-gap: ${rowGap}`};
-  ${(areas: string) => areas && `grid-template-areas: ${formatAreas(areas)}`};
-  ${(justifyContent: string) => justifyContent && `justify-content: ${justifyContent}`};
-  ${(alignContent: string) => alignContent && `align-content: ${alignContent}`};
+  grid-template-columns: ${({ columns = 12 }: Record<string, any>) => frGetter(columns)};
+  grid-gap: ${({ gap = '8px' }: Record<string, any>) => gap};
+  ${(columnGap: Record<string, any>) => columnGap && `column-gap: ${columnGap}`};
+  ${(rowGap: Record<string, any>) => rowGap && `row-gap: ${rowGap}`};
+  ${(areas: Record<string, any>) => areas && `grid-template-areas: ${formatAreas(areas)}`};
+  ${(justifyContent: Record<string, any>) =>
+    justifyContent && `justify-content: ${justifyContent}`};
+  ${(alignContent: Record<string, any>) => alignContent && `align-content: ${alignContent}`};
 `;
-
 export default GridContainer;
