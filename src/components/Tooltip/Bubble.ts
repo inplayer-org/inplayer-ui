1cf947795d723207ccccd1dc14552e808acab3ae
mustran <ademimusli000@gmail.com>
2020-03-24 13:56:40 +0100

    Rewrite Bubble styles using TypeScript

---
+++ src/components/Tooltip/Bubble.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+import colors from 'theme/colors';
+import { fontSizes } from 'utils';
+
+type BubbleProps = {
+  background?: string;
+  radius?: number;
+  border: string;
+  padding?: number;
+  fontSize?: number;
+  fontFamily: string;
+};
+
+const Bubble = styled('div')<BubbleProps>`
+  color: ${colors.white};
+  background: ${({ background }) => background || colors.blue};
+  border-radius: ${({ radius }) => `${radius}px` || '0.3em'};
+  border: 1px solid ${({ border }) => `${border}`};
+  padding: ${({ padding }) => `${padding}rem` || 0};
+  font-size: ${({ fontSize }) => fontSize || fontSizes('small')};
+  font-family: ${({ fontFamily }) => fontFamily};
+  line-height: 1.4;
+`;
+
+export default Bubble;
