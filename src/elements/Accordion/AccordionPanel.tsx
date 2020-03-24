e9419edd5b658c1e79ce1329011affb767b0ea79
mustran <ademimusli000@gmail.com>
2020-03-24 10:20:54 +0100

    Rewrite AccordionPanel using TypeScript

---
+++ src/elements/Accordion/AccordionPanel.tsx
@@ -0,0 +1,158 @@
+import React, { SyntheticEvent } from 'react';
+import styled, { css } from 'styled-components';
+import colors from 'theme/colors';
+import { ifProp, ifNotProp } from 'styled-tools';
+import { uiColors, fontWeights } from 'utils';
+
+// Components
+import Tooltip, { Props as TooltipProps } from 'components/Tooltip';
+import { FaAngleUp, FaAngleDown } from 'react-icons/fa';
+import Typography from '../Typography';
+
+type AccordionPanelContainerProps = {
+  isActive: boolean;
+  contentHeight: string;
+};
+
+const AccordionPanelContainer = styled.div<AccordionPanelContainerProps>`
+  width: 100%;
+  height: 3.5rem;
+  box-sizing: border-box;
+  overflow: hidden;
+  z-index: 10;
+  background: ${colors.white};
+  transition: ease 500ms height;
+  ${ifProp(
+    'isActive',
+    css`
+      position: absolute;
+      ${({ contentHeight }: AccordionPanelContainerProps) =>
+        contentHeight && `height: calc(${contentHeight} - 11rem)`};
+      top: 0;
+      z-index: 30;
+      border-bottom: none;
+      overflow: auto;
+      margin-top: 3.5rem;
+      display: block;
+    `,
+    css`
+      height: 0;
+    `
+  )}
+`;
+
+type AccordionPanelHeaderProps = {
+  isActive: boolean;
+  disabled: boolean;
+  onClick: any;
+};
+
+const AccordionPanelHeader = styled.header<AccordionPanelHeaderProps>`
+  margin: 0;
+  padding: 1rem 3%;
+  width: 100%;
+  box-sizing: border-box;
+  background: ${ifProp('disabled', colors.lightGray, colors.white)};
+  cursor: ${({ disabled }) => (disabled ? 'default' : 'pointer')};
+  border-bottom: 1px solid ${colors.lightGray};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  ${ifProp(
+    'isActive',
+    css`
+      position: absolute;
+      top: 0;
+    `
+  )}
+`;
+
+type AccordionTitleProps = {
+  isActive: boolean;
+  disabled: boolean;
+};
+
+const AccordionTitle = styled(Typography)<AccordionTitleProps>`
+  font-weight: ${fontWeights('thin')};
+  color: ${ifProp('disabled', colors.fontLightGray, colors.fontDarkGray)};
+  margin: 0;
+  display: inline;
+`;
+
+const createIconStyles = () => ({
+  color: colors.fontGray,
+  'margin-left': 'auto',
+});
+
+const StyledAngleDown = styled(FaAngleDown)`
+  ${createIconStyles}
+`;
+
+const StyledAngleUp = styled(FaAngleUp)`
+  ${createIconStyles}
+`;
+
+const AccordionPanelDetails = styled.div`
+  padding: 0;
+  height: 100%;
+`;
+
+type AccordionIconHolderProps = {
+  isAccordionDisabled: boolean;
+};
+
+const AccordionIconHolder = styled.div<AccordionIconHolderProps>`
+  ${ifNotProp(
+    'isAccordionDisabled',
+    css`
+      display: flex;
+      width: 3rem;
+      justify-content: space-beetween;
+    `
+  )}
+`;
+
+type Props = {
+  label: string;
+  isActive: boolean;
+  isOtherPanelActive: boolean;
+  icon?: Node;
+  iconTooltip?: TooltipProps;
+  contentHeight: string;
+  togglePanel: (e?: SyntheticEvent) => void;
+  renderContent: (actions: { closePanel: (e?: SyntheticEvent) => void }) => any;
+  closePanel: (e?: SyntheticEvent) => void;
+  disabled: boolean;
+};
+
+const AccordionPanel = ({
+  label,
+  isActive,
+  isOtherPanelActive,
+  icon,
+  iconTooltip,
+  renderContent,
+  contentHeight,
+  disabled,
+  togglePanel,
+  closePanel,
+}: Props) => (
+  <>
+    <AccordionPanelHeader
+      disabled={disabled}
+      onClick={!disabled ? togglePanel : null}
+      isActive={isActive}
+    >
+      <AccordionTitle variant="h6" isActive={isActive} disabled={disabled}>
+        {label}
+      </AccordionTitle>
+      <AccordionIconHolder isAccordionDisabled={disabled}>
+        {!isOtherPanelActive && (iconTooltip ? <Tooltip {...iconTooltip}>{icon}</Tooltip> : icon)}
+        {!disabled && (isActive ? <StyledAngleUp /> : <StyledAngleDown />)}
+      </AccordionIconHolder>
+    </AccordionPanelHeader>
+    <AccordionPanelContainer isActive={isActive} contentHeight={contentHeight}>
+      <AccordionPanelDetails>{isActive && renderContent({ closePanel })}</AccordionPanelDetails>
+    </AccordionPanelContainer>
+  </>
+);
