ee866705e10849f6115f2ee6a2eeebb2db986f61
mustran <ademimusli000@gmail.com>
2020-03-25 12:37:20 +0100

    Remove unused type

--- src/elements/Button/Button.tsx
+++ src/elements/Button/Button.tsx
@@ -1,4 +1,4 @@
-import React, { CSSProperties, ReactChild } from 'react';
+import React, { CSSProperties } from 'react';
 import styled from 'styled-components';
 import ButtonWrapper from './ButtonWrapper';
 
