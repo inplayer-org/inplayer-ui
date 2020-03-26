4b61a61cdc1b74b14c1ed735e59745346c7f1a8a
mustran <ademimusli000@gmail.com>
2020-03-26 12:44:33 +0100

    Add Usage for testing

---
+++ src/elements/Label/Usage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Label } from './index';
+
+const Usage = () => {
+  return (
+    <>
+      <Label> Label </Label>
+      <br />
+      <Label disabled> Label disabled </Label>
+    </>
+  );
+};
+export default Usage;
