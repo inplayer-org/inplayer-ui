39ba683c83dbac5abbacc22f53bf5e0a700db3cc
mustran <ademimusli000@gmail.com>
2020-03-27 10:11:42 +0100

    Export Usage as default

---
+++ src/elements/Notification/Usage.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Notification } from '.';
+
+const Usage = () => (
+  <>
+    <button
+      onClick={() =>
+        Notification.create({
+          title: 'Notification',
+          content: 'This is a regular notification',
+          duration: 3,
+        })
+      }
+    >
+      REGULAR NOTIFICATION
+    </button>
+    <button
+      onClick={() =>
+        Notification.create({
+          title: 'Hooray! ',
+          variant: 'success',
+          content: 'This is a success notification',
+          duration: 3,
+        })
+      }
+    >
+      SUCCESS NOTIFICATION
+    </button>
+    <button
+      onClick={() =>
+        Notification.create({
+          title: 'Warning! ',
+          variant: 'warning',
+          content: 'This is a warning notification',
+          duration: 3,
+        })
+      }
+    >
+      WARNING NOTIFICATION
+    </button>
+    <button
+      onClick={() =>
+        Notification.create({
+          title: 'Danger! ',
+          variant: 'danger',
+          content: 'This is a danger notification',
+          duration: 3,
+          close: () => console.log('CLOSE'),
+        })
+      }
+    >
+      DANGER NOTIFICATION
+    </button>
+  </>
+);
+
+export default Usage;
