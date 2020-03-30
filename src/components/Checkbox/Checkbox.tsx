7a8ef1c7be8ef59e29e1a6ef09e7ad4d02ceec5d
mustran <musli.ademi@inplayer.com>
2020-03-30 13:27:35 +0200

    Fix checked/unchecked bug

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent, RefObject } from 'react';
+import { CSSProperties } from 'styled-components';
 import { Label } from '../../elements/Label';
 import CheckboxWrapper from './CheckboxWrapper';
-import { CSSProperties } from 'styled-components';
 
 type Props = {
   label: string;
@@ -25,12 +25,11 @@ const Checkbox = ({
   disabled = false,
   ...rest
 }: Props) => {
-  const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>): any =>
-    onChange(e.target.checked);
+  const onCheckboxChange = (e: ChangeEvent<HTMLInputElement>): any => onChange(e.target.checked);
 
   return (
     <CheckboxWrapper className={className} style={style} ref={containerRef}>
-      <input type="checkbox" id={id} onChange={onCheckboxChange} {...rest} />
+      <input disabled={disabled} type="checkbox" id={id} onChange={onCheckboxChange} {...rest} />
       <Label htmlFor={id}>{label}</Label>
     </CheckboxWrapper>
   );
