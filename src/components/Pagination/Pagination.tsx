9078d6016d62a1c9dba6b3061785e4c59b267fea
mustran <musli.ademi@inplayer.com>
2020-04-02 12:39:32 +0200

    Rewrite Pagination

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,79 +1,14 @@
 // @flow
 import React, { useState, useEffect } from 'react';
-import styled, { css } from 'styled-components';
-import colors from 'config/colors';
-import { ifProp } from 'styled-tools';
 import { FaAngleDoubleLeft, FaAngleDoubleRight, FaAngleLeft, FaAngleRight } from 'react-icons/fa';
-
-const PaginationContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const getBoxStyles = ({
-  disabled = false,
-  selected = false,
-}: {
-  disabled?: boolean,
-  selected?: boolean,
-  hideBorder?: boolean,
-}) => {
-  if (disabled) {
-    return css`
-      cursor: not-allowed;
-      border: 1px solid ${colors.gray};
-      color: ${colors.gray};
-    `;
-  }
-  if (selected) {
-    return css`
-      border: 1px solid ${colors.skyBlue};
-      color: ${colors.white};
-      background-color: ${colors.skyBlue};
-    `;
-  }
-  return css`
-    :hover {
-      border: 1px solid ${colors.skyBlue};
-      ${ifProp(
-        'hideBorder',
-        css`
-          border: none;
-        `
-      )}
-      color: ${colors.skyBlue};
-      font-weight: bold;
-    }
-  `;
-};
-
-const PageBox = styled.button`
-  width: 2rem;
-  height: 2rem;
-  border-radius: 4px;
-  margin: 0 0.2rem;
-  text-align: center;
-  border: 1px solid ${colors.gray};
-  color: rgba(0, 0, 0, 0.65);
-  cursor: pointer;
-  transition: all 0.3s ease;
-  background-color: ${colors.white};
-  ${ifProp(
-    'hideBorder',
-    css`
-      border: none;
-      background-color: transparent;
-    `
-  )}
-  ${getBoxStyles}
-`;
+import { PageBox, PaginationContainer, getBoxStyles } from './styled';
 
 type Props = {
-  onPageChange: (pageNumber: number) => any,
-  totalItems: number,
-  startPage?: number,
-  numberOfPagesDisplayed?: number,
-  itemsPerPage?: number,
+  onPageChange: (pageNumber: number) => void;
+  totalItems: number;
+  startPage?: number;
+  numberOfPagesDisplayed?: number;
+  itemsPerPage?: number;
 };
 
 // Kibana has limit of 9990 pages
---
+++ src/components/Pagination/styled.ts
@@ -0,0 +1,66 @@
+import styled, { css } from 'styled-components';
+import colors from 'theme/colors';
+import { ifProp } from 'styled-tools';
+
+export const PaginationContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const getBoxStyles = ({
+  disabled = false,
+  selected = false,
+}: {
+  disabled?: boolean;
+  selected?: boolean;
+  hideBorder?: boolean;
+}) => {
+  if (disabled) {
+    return css`
+      cursor: not-allowed;
+      border: 1px solid ${colors.gray};
+      color: ${colors.gray};
+    `;
+  }
+  if (selected) {
+    return css`
+      border: 1px solid ${colors.skyBlue};
+      color: ${colors.white};
+      background-color: ${colors.skyBlue};
+    `;
+  }
+  return css`
+  :hover {
+    border: 1px solid ${colors.skyBlue};
+    ${ifProp(
+      'hideBorder',
+      css`
+        border: none;
+      `
+    )}
+    color: ${colors.skyBlue};
+    font-weight: bold;
+  }
+`;
+};
+
+export const PageBox = styled.button`
+  width: 2rem;
+  height: 2rem;
+  border-radius: 4px;
+  margin: 0 0.2rem;
+  text-align: center;
+  border: 1px solid ${colors.gray};
+  color: rgba(0, 0, 0, 0.65);
+  cursor: pointer;
+  transition: all 0.3s ease;
+  background-color: ${colors.white};
+  ${ifProp(
+    'hideBorder',
+    css`
+      border: none;
+      background-color: transparent;
+    `
+  )}
+  ${getBoxStyles}
+`;
