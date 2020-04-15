ad62f820dec50c303966abd81eb03a11b5c15556
mustran <musli.ademi@inplayer.com>
2020-04-15 16:23:41 +0200

    Add docs for Navbar component

--- src/components/NavBar/Usage.tsx
+++ src/components/NavBar/Usage.tsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import { IoMdAnalytics } from 'react-icons/io';
 import { MdPhotoLibrary, MdPeople } from 'react-icons/md';
-import { Navbar } from '.';
+import Navbar from '.';
 
-const Usage = () => {
-  return (
-    <Navbar logo="https://inplayer.com/wp-content/themes/darkwin/pics/brand/android-icon-192x192.png">
-      <Navbar.MenuItem
-        icon={<IoMdAnalytics size="192x192" />}
-        onClick={() => console.log('Analytics clicked')}
-        active
-      >
-        Analytics
-      </Navbar.MenuItem>
-      <Navbar.MenuItem
-        icon={<MdPhotoLibrary size="192x192" />}
-        onClick={() => console.log('Assets clicked')}
-      >
-        Assets
-      </Navbar.MenuItem>
-      <Navbar.MenuItem
-        icon={<MdPeople size="192x192" />}
-        onClick={() => console.log('Audience clicked')}
-      >
-        Audience
-      </Navbar.MenuItem>
-    </Navbar>
-  );
-};
+const Usage = () => (
+  <Navbar logo="https://inplayer.com/wp-content/themes/darkwin/pics/brand/android-icon-192x192.png">
+    <Navbar.MenuItem
+      icon={<IoMdAnalytics size="192x192" />}
+      onClick={() => console.log('Analytics clicked')}
+      active
+    >
+      Analytics
+    </Navbar.MenuItem>
+    <Navbar.MenuItem
+      icon={<MdPhotoLibrary size="192x192" />}
+      onClick={() => console.log('Assets clicked')}
+    >
+      Assets
+    </Navbar.MenuItem>
+    <Navbar.MenuItem
+      icon={<MdPeople size="192x192" />}
+      onClick={() => console.log('Audience clicked')}
+    >
+      Audience
+    </Navbar.MenuItem>
+  </Navbar>
+);
 
 export default Usage;
