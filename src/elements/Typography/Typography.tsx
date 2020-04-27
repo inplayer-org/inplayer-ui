397a330f76800d5a321b8eb04d36e29a0779b799
srdjanRakic <srgjan.rakic@gmail.com>
2020-04-27 12:21:36 +0200

    fix: typography theme prop

--- src/elements/Typography/Typography.tsx
+++ src/elements/Typography/Typography.tsx
@@ -37,7 +37,7 @@ type HeadingProps = {
 
 const Paragraph = styled.p<ParagraphProps>`
   font-size:${({ theme }) => theme.font.sizes.medium};
-  color: ${({ color }) => color || `${({ theme }) => theme.palette.text.main}`};
+  color: ${({ color, theme }) => color || `${theme.palette.text.main}`};
   ${({ description, theme: { palette, font } }) =>
     description &&
     `
