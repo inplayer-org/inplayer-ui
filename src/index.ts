a8c7997949efad964752e614c30ec920e844d2e8
srdjanRakic <srgjan.rakic@gmail.com>
2020-05-04 20:06:49 +0200

    chore: rename previews folder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export * from './components';
-export * from './previews';
+export * from './paywall-previews';
 export { default as ThemeWrapper } from 'theme/ThemeWrapper';
 export { default as Colors } from 'theme/colors';
