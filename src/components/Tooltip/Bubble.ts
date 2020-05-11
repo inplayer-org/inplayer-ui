c94160ba20ac17d0262291159f958b5ce95a6b92
mustran <musli.ademi@inplayer.com>
2020-05-11 14:44:47 +0200

    Desctructure props

--- src/components/Tooltip/Bubble.ts
+++ src/components/Tooltip/Bubble.ts
@@ -12,15 +12,15 @@ type BubbleProps = {
 
 const Bubble = styled.div<BubbleProps>`
   color: ${colors.white};
-  background: ${(props) => props.background || props.theme.palette.secondary.main};
+  background: ${({ background, theme }) => background || theme.palette.secondary.main};
   border-radius: ${({ radius }) => (radius ? `${radius}px` : '0.3em')};
-  border: ${(props) =>
+  border: ${({ border, background, theme }) =>
     // eslint-disable-next-line no-nested-ternary
-    props.border
-      ? `1px solid ${props.border};`
-      : props.background
-      ? `1px solid ${props.background};`
-      : props.theme.palette.secondary.main};
+    border
+      ? `1px solid ${border};`
+      : background
+      ? `1px solid ${background};`
+      : theme.palette.secondary.main};
   padding: ${({ padding }) => `${padding}rem` || 0};
   font-size: ${({ fontSize, theme }) => fontSize || theme.font.sizes.small};
   font-family: ${({ fontFamily }) => fontFamily};
