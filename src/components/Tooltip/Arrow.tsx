9ccd870be798d7ea14b323186834c8b11844fa10
mustran <musli.ademi@inplayer.com>
2020-05-11 12:58:13 +0200

    Fix tooltip arrow and border styles problem

--- src/components/Tooltip/Arrow.tsx
+++ src/components/Tooltip/Arrow.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import colors from 'theme/colors';
 
 type ArrowsProps = {
   width: number;
@@ -11,22 +10,22 @@ type ArrowsProps = {
 
 const Base = styled.div<ArrowsProps>`
   position: absolute;
-  ${({ width }) => width && `width: ${width}px`};
-  ${({ width }) => width && `height: ${width}px`};
-  background: ${({ background }) => background || colors.blue};
+  ${({ width }) => width && `width: ${width}px;`};
+  ${({ width }) => width && `height: ${width}px;`};
+  background: ${(props) => props.background || props.theme.palette.secondary.main};
 `;
 
 const Up = styled(Base)`
   transform: translateX(-50%) translateY(50%) rotateZ(45deg);
-  bottom: 98%;
+  bottom: 100%;
   left: 50%;
-  ${({ border }) => border && `border-left: 1px solid ${border}`}
-  ${({ border }) => border && `border-top: 1px solid ${border}`}
+  ${({ border }) => border && `border-left: 1px solid ${border};`}
+  ${({ border }) => border && `border-top: 1px solid ${border};`}
 `;
 
 const Down = styled(Base)`
   transform: translateX(-50%) translateY(-50%) rotateZ(45deg);
-  top: 98%;
+  top: 100%;
   left: 50%;
   ${({ border }) => border && `border-right: 1px solid ${border}`}
   ${({ border }) => border && `border-bottom: 1px solid ${border}`}
@@ -34,7 +33,7 @@ const Down = styled(Base)`
 
 const Left = styled(Base)`
   transform: translateX(50%) translateY(-50%) rotateZ(45deg);
-  right: 99%;
+  right: 100%;
   top: 50%;
   ${({ border }) => border && `border-left: 1px solid ${border}`}
   ${({ border }) => border && `border-bottom: 1px solid ${border}`}
@@ -42,7 +41,7 @@ const Left = styled(Base)`
 
 const Right = styled(Base)`
   transform: translateX(-50%) translateY(-50%) rotateZ(45deg);
-  left: 99%;
+  left: 100%;
   top: 50%;
   ${({ border }) => border && `border-right: 1px solid ${border}`}
   ${({ border }) => border && `border-top: 1px solid ${border}`}
--- src/components/Tooltip/Bubble.ts
+++ src/components/Tooltip/Bubble.ts
@@ -4,7 +4,7 @@ import colors from 'theme/colors';
 type BubbleProps = {
   background?: string;
   radius?: number;
-  border: string;
+  border?: string;
   padding?: number;
   fontSize?: string;
   fontFamily: string;
@@ -12,9 +12,15 @@ type BubbleProps = {
 
 const Bubble = styled.div<BubbleProps>`
   color: ${colors.white};
-  background: ${({ background }) => background || colors.blue};
-  border-radius: ${({ radius }) => `${radius}px` || '0.3em'};
-  border: 1px solid ${({ border }) => `${border}`};
+  background: ${(props) => props.background || props.theme.palette.secondary.main};
+  border-radius: ${({ radius }) => (radius ? `${radius}px` : '0.3em')};
+  border: ${(props) =>
+    // eslint-disable-next-line no-nested-ternary
+    props.border
+      ? `1px solid ${props.border};`
+      : props.background
+      ? `1px solid ${props.background};`
+      : props.theme.palette.secondary.main};
   padding: ${({ padding }) => `${padding}rem` || 0};
   font-size: ${({ fontSize, theme }) => fontSize || theme.font.sizes.small};
   font-family: ${({ fontFamily }) => fontFamily};
