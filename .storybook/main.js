e2e215fe99995e581ab2ce623213059fa8ff1072
srdjanRakic <srgjan.rakic@gmail.com>
2020-05-14 19:46:37 +0200

    chore: use custom webpack config for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,33 @@
+const path = require('path');
+
+// your app's webpack.config.js
+const custom = require('../webpack.prod.js');
+
 module.exports = {
   stories: ['../src/**/*.stories.(tsx|mdx)'],
   addons: ['@storybook/addon-docs'],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
+      use: [
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
     });
+
     config.resolve.extensions.push('.ts', '.tsx');
-    return config;
+
+    return {
+      ...config,
+      resolve: custom.resolve,
+    };
   },
+  // webpackFinal: (config) => {
+  //   return {
+  //     ...config,
+  //     module: { ...config.module, rules: custom.module.rules },
+  //     resolve: custom.resolve,
+  //   };
+  // },
 };
