b059df6a1df639efead3bdccd562174af4551d6c
Elena Stojanova <elena.stojanova@inplayer.com>
2020-05-15 15:00:50 +0200

    Revert old preview 1 css

--- src/paywall-previews/Preview1/Preview1.tsx
+++ src/paywall-previews/Preview1/Preview1.tsx
@@ -1,159 +1,119 @@
 import React from 'react';
 import colors from 'theme/colors';
-import styled from 'styled-components';
 
 // Images
 import previewImg from 'assets/images/ip-preview-premium.png';
-import restrictedAssetImg from 'assets/images/restricted-asset.png';
-import logo from 'assets/images/ip-icon-blue.svg';
 
 // Types
 import Branding from '../types/branding';
 
 // Components
-import TextEditor from '../shared/TextEditor';
-import { OverlayLabel, PreviewImage, FooterLink } from '../shared/PreviewComponents';
+import { OverlayLabel } from '../shared/PreviewComponents';
 import {
-  StyledPreviewBox,
-  StyledImageHolder,
-  StyledPaywallDescription,
-  PremiumContent,
   LockIcon,
-  PaywallDescriptionSpan,
-  Protected,
-  Logo,
+  InplayerPreviewBox,
+  ImageHolder,
+  PaywallExplain,
+  InplayerWhiteLogo,
   ItemDetails,
-  Title,
   BuyButtonWrapper,
-  StyledBuyButton,
-  StyledPreviewFooter,
+  BuyButton,
+  InplayerFooter,
+  PreviewImage,
 } from './styled';
 
 type Props = {
-  /**
-   * Branding type
-   */
   branding?: Branding;
-  /**
-   * Whether to show the button or not
-   */
   displayBuyButton?: boolean;
-  /**
-   * Preview unavailable
-   */
   previewUnavailable?: boolean;
-  /**
-   * Minimum width
-   */
   minWidth?: string;
-  /**
-   * Preview height
-   */
   height?: string;
-  /**
-   * Preview width
-   */
   width?: string;
-  /**
-   * Login footer label
-   */
   loginFooterLabel?: string;
-  /**
-   * Whether the asset is restricted or not
-   */
-  isRestrictedAsset?: boolean;
-  /**
-   * Function which handles the modal state changes
-   */
   handleOpenModal?: (e: any) => any;
+  protectedByLabel: boolean;
+  premiumContentLabel: string;
 };
-
-const FlexContainer = styled.div`
-  display: flex;
-  flex-wrap: nowrap;
-  align-items: center;
-  @media (max-width: 768px) {
-    flex-direction: column;
-  }
-`;
 
 const Preview1 = ({
   branding: {
     paywall_cover_photo: imageUrl = previewImg,
-    preview_title: previewTitle = `<h3>Asset title</h3>`,
-    preview_description: previewDescription = `<p>Asset description</p>`,
-    preview_button_label: previewButtonLabel = 'Buy',
-    preview_top_border: hasPreviewTopBorder = true,
+    preview_title: previewTitle,
+    preview_description: previewDescription,
+    preview_button_label: previewButtonLabel,
+    preview_top_border: hasPreviewTopBorder = false,
     inplayer_protected_label: hasProtectedByLabel = true,
     preview_buttons_bg_color: buttonBgColor = colors.green,
     preview_buttons_text_color: buttonTextColor = colors.white,
   } = {},
   displayBuyButton = true,
   previewUnavailable = false,
-  width = '',
-  minWidth = '',
-  height = '',
-  isRestrictedAsset = false,
-  loginFooterLabel = 'Already have access? Login with your InPlayer account',
+  loginFooterLabel,
   handleOpenModal,
+  protectedByLabel,
+  premiumContentLabel,
 }: Props) => {
-  const image = isRestrictedAsset ? restrictedAssetImg : imageUrl;
+  const isHtmlTitle = previewTitle.includes('</');
+
   return (
-    <StyledPreviewBox
-      minWidth={minWidth}
-      width={width}
-      height={height}
-      hasPreviewTopBorder={hasPreviewTopBorder}
-      className="inplayer-preview-box"
-    >
+    <InplayerPreviewBox hasPreviewTopBorder={hasPreviewTopBorder} className="inplayer-preview-box">
       {previewUnavailable && <OverlayLabel variant="h5">Preview not available yet</OverlayLabel>}
-      <StyledImageHolder className="inplayer-imageholder">
-        <PreviewImage src={image} />
-        <StyledPaywallDescription
-          className="inplayer-paywallexplain"
+      <ImageHolder className="inplayer-imageholder">
+        <PreviewImage
+          alt="coverPhoto"
+          src={imageUrl}
+          role="presentation"
+          className="inplayer-paywall"
+        />
+        <PaywallExplain
           hasProtectedByLabel={hasProtectedByLabel}
-          color={colors.white}
+          className="inplayer-paywallexplain"
         >
-          <PremiumContent>
-            <LockIcon name="lock" />
-            <PaywallDescriptionSpan>
-              This premium content requires an account to access.
-            </PaywallDescriptionSpan>
-          </PremiumContent>
-          <PremiumContent>
-            <Protected>Protected by InPlayer</Protected>
-            <a href="https://inplayer.com" target="_blank" rel="noopener noreferrer">
-              <Logo src={logo} alt="logo" />
-            </a>
-          </PremiumContent>
-        </StyledPaywallDescription>
-      </StyledImageHolder>
-      <FlexContainer>
-        <ItemDetails className="inplayer-itemdetails">
-          <Title variant="h3" className="inplayer-title">
-            <TextEditor value={previewTitle} readOnly />
-          </Title>
-          <TextEditor value={previewDescription} readOnly />
-        </ItemDetails>
-        {displayBuyButton && (
-          <BuyButtonWrapper className="inplayer-buybutton">
-            <StyledBuyButton
-              className="inplayer-button"
-              buttonBgColor={buttonBgColor}
-              buttonTextColor={buttonTextColor}
-              onClick={handleOpenModal}
-            >
-              {previewButtonLabel}
-            </StyledBuyButton>
-          </BuyButtonWrapper>
+          <InplayerWhiteLogo id="inplayer-whitelogo">
+            <strong>{protectedByLabel} inplayer</strong>
+            <a href="https://inplayer.com">InPlayer Paywall</a>
+          </InplayerWhiteLogo>
+          <LockIcon className="inplayer-lock" />
+          <span>{premiumContentLabel}</span>
+        </PaywallExplain>
+      </ImageHolder>
+      <ItemDetails className="inplayer-itemdetails">
+        {isHtmlTitle ? (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: previewTitle,
+            }}
+            className="inplayer-title"
+          />
+        ) : (
+          <h3 className="inplayer-title">{previewTitle}</h3>
         )}
-      </FlexContainer>
-      <StyledPreviewFooter className="inplayer-preview-footer" color={colors.fontGray}>
-        <FooterLink href="#login" onClick={handleOpenModal}>
-          {loginFooterLabel}
-        </FooterLink>
-      </StyledPreviewFooter>
-    </StyledPreviewBox>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: previewDescription,
+          }}
+        />
+      </ItemDetails>
+      {displayBuyButton && (
+        <BuyButtonWrapper className="inplayer-buybutton">
+          <BuyButton
+            className="inplayer-button inplayer-submit-button"
+            buttonBgColor={buttonBgColor}
+            buttonTextColor={buttonTextColor}
+            onClick={handleOpenModal}
+          >
+            {previewButtonLabel}
+          </BuyButton>
+        </BuyButtonWrapper>
+      )}
+      <InplayerFooter className="inplayer-preview-footer">
+        <div className="inplayer-guest-footer">
+          <a href="#login" onClick={handleOpenModal}>
+            <span>{loginFooterLabel}</span>
+          </a>
+        </div>
+      </InplayerFooter>
+    </InplayerPreviewBox>
   );
 };
 
--- src/paywall-previews/Preview1/styled.ts
+++ src/paywall-previews/Preview1/styled.ts
@@ -1,115 +1,222 @@
 import styled from 'styled-components';
+import { ifProp } from 'styled-tools';
 import { transparentize } from 'polished';
+import colors from 'theme/colors';
 import { FaLock } from 'react-icons/fa';
 
-// utils
-import colors from 'theme/colors';
-
-// Components
-import Typography from 'components/Typography';
-import {
-  PreviewFooter,
-  PreviewBox,
-  ImageHolder,
-  PaywallDescription,
-  DescriptionSpan,
-  BuyButton,
-} from '../shared/PreviewComponents';
-
-export const StyledPreviewBox = styled(PreviewBox)`
-  max-width: ${({ width }) => (width ? '' : '70vh')};
-  border-radius: 8px 8px 3px 3px;
-`;
-
-export const StyledImageHolder = styled(ImageHolder)`
-  height: 320px;
-  min-height: 220px;
-`;
-
-interface StyledPaywallDescriptionProps {
-  hasProtectedByLabel: boolean;
+interface PreviewBoxProps {
+  hasPreviewTopBorder: boolean;
 }
 
-export const StyledPaywallDescription = styled(PaywallDescription)<StyledPaywallDescriptionProps>`
-  background: ${transparentize(0.6, colors.black)};
+export const InplayerPreviewBox = styled.div<PreviewBoxProps>`
+  width: 100%;
+  max-width: 100vh;
+  background: ${ifProp(
+    'hasPreviewTopBorder',
+    `url('https://assets.inplayer.com/images/paywall/ip-graphic-border.jpg') top no-repeat ${colors.white}`,
+    `${colors.white}`
+  )};
+  margin: 24px auto;
   box-sizing: border-box;
-  padding: 3% 3%;
+  border: 1px solid #e9edef;
+  clear: both;
+  background-size: 100% 4px;
+  position: relative;
+  -webkit-border-radius: 3px;
+  -moz-border-radius: 3px;
+  border-radius: 3px;
+  overflow: hidden;
+  font-size: 16px;
+  line-height: 20px;
+  color: ${colors.black};
+  text-align: left;
+  font-weight: 300;
+  font-family: 'Segoe UI', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+  padding: 0;
+
+  @media screen and (max-height: 600px) {
+    max-width: 580px;
+  }
+`;
+
+export const ImageHolder = styled.div`
+  width: 100%;
+  background: #def;
+  height: 0;
+  padding-bottom: 56.25%;
+  overflow: hidden;
+  display: block;
+  position: relative;
+`;
+
+export const PaywallExplain = styled.div<{ hasProtectedByLabel: boolean }>`
+  background: rgba(58, 71, 76, 0.6);
+  box-sizing: border-box;
+  padding: 2vh 3%;
+  color: ${colors.white};
+  display: block;
   position: absolute;
   bottom: 0;
   width: 100%;
+  font-size: 13px;
   line-height: 15px;
-  display: ${({ hasProtectedByLabel }) => (hasProtectedByLabel ? 'flex' : 'none')};
-  justify-content: space-between;
-  vertical-align: middle;
-  align-items: center;
+  text-align: left;
+
+  span {
+    vertical-align: middle;
+    max-width: calc(100% - 150px);
+    display: inline-block;
+    text-align: left;
+    margin: 0;
+    position: relative;
+    font-size: 13px;
+    padding: 0;
+    line-height: 15px;
+  }
 `;
 
-export const PremiumContent = styled.div`
-  display: flex;
-  align-items: center;
+export const InplayerWhiteLogo = styled.div`
+  position: absolute;
+  right: 3%;
+  vertical-align: middle;
+  display: block;
+  margin: -4px 0 0;
+  padding: 0;
+  line-height: 15px;
+
+  strong {
+    vertical-align: middle;
+    font-size: 12px;
+    font-family: 'Roboto Condensed', sans-serif;
+    text-transform: uppercase;
+    display: inline-block;
+    color: rgba(255, 255, 255, 0.8);
+    font-weight: normal;
+    max-width: 150px;
+    text-align: right;
+    line-height: 15px;
+
+    @media screen and (max-width: 600px) {
+      max-width: 73px;
+    }
+  }
+
+  a {
+    background: url('https://assets.inplayer.com/images/paywall/ip-logicon.svg') no-repeat center
+      center;
+    height: 30px;
+    width: 30px;
+    background-size: 30px;
+    display: inline-block;
+    text-indent: -9999vw;
+    vertical-align: middle;
+    margin-left: 7px;
+    margin-bottom: 2px;
+    line-height: 15px;
+  }
+`;
+
+export const ItemDetails = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  width: 69%;
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  h7 {
+    margin: 20px 0;
+    color: rgba(0, 0, 0, 0.7);
+    text-align: center;
+    font-weight: 300;
+  }
+
+  a {
+    text-decoration: underline;
+  }
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+  }
+
+  @media screen and (min-width: 600px) {
+    width: 66%;
+    padding: 0 10px;
+  }
+`;
+
+export const BuyButtonWrapper = styled.div`
+  display: inline-block;
+  vertical-align: middle;
+  text-align: right;
+  width: 30%;
+
+  @media screen and (max-width: 600px) {
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+interface ButtonProps {
+  buttonBgColor: string;
+  buttonTextColor: string;
+}
+
+export const BuyButton = styled.button<ButtonProps>`
+  vertical-align: middle;
+  border-radius: 2px;
+  box-sizing: border-box;
+  overflow: hidden;
+  outline: none;
+  padding: 13px 25px;
+  background: ${({ buttonBgColor }) => transparentize(0.2, buttonBgColor)};
+  text-transform: uppercase;
+  color: ${({ buttonTextColor }) => buttonTextColor};
+  font-weight: bold;
+  border: none;
+  font-size: 16px;
+  cursor: pointer;
+  min-width: 80%;
+  margin-bottom: 30px;
+  margin-top: 14px;
+  transition: background ease 500ms;
+  width: 100%;
+  letter-spacing: 0.04em;
+  font-family: Roboto, Helvetica Neue, Helvetica, Arial, sans-serif;
+  transform: translateY(0);
+
+  &:hover,
+  &:focus {
+    outline: none;
+    transform: translateY(-2px);
+    box-shadow: 0 1px 5px 1px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+export const InplayerFooter = styled.div`
+  border-top: 1px solid #e9edef;
+  margin-top: 10px;
+  text-align: center;
+  padding: 15px 0;
+  font-size: 14px;
+
+  a {
+    text-decoration: none;
+    color: #aaa;
+    font-size: 14px;
+    line-height: 18px;
+  }
+`;
+
+export const PreviewImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+  object-position: 50% 50%;
 `;
 
 export const LockIcon = styled(FaLock)`
   margin: 0 0.4em 0 0;
-`;
-
-export const PaywallDescriptionSpan = styled(DescriptionSpan)`
-  font-size: ${({ theme }) => theme.font.sizes.extraSmall};
-  width: 100%;
-`;
-
-export const Protected = styled.strong`
-  font-size: ${({ theme }) => theme.font.sizes.small};
-  font-family: 'Roboto Condensed', sans-serif;
-  text-transform: uppercase;
-  color: ${({ theme }) => theme.palette.text.disabled};
-  font-weight: normal;
-  text-align: right;
-`;
-
-export const Logo = styled.img`
-  height: 30px;
-  width: 30px;
-  background-size: 30px;
-  display: inline-block;
-  text-indent: -9999vw;
-  vertical-align: middle;
-  margin-left: 7px;
-  line-height: 15px;
-`;
-
-export const ItemDetails = styled.div`
-  flex-basis: 70%;
-  @media screen and (max-width: 1100px) {
-    width: 100%;
-  }
-`;
-
-export const Title = styled(Typography)`
-  overflow-wrap: break-word;
-  font-weight: ${({ theme }) => theme.font.weights.normal};
-  font-size: ${({ theme }) => theme.font.sizes.medium};
-  line-height: 1.3em;
-  color: ${colors.black};
-  margin: 0;
-`;
-
-export const BuyButtonWrapper = styled.div`
-  flex-basis: 30%;
-  @media (max-width: 768px) {
-    flex-basis: 100%;
-  }
-`;
-
-export const StyledBuyButton = styled(BuyButton)`
-  margin-bottom: 1.8em;
-  padding: 0.85em 4em;
-  float: right;
-`;
-
-export const StyledPreviewFooter = styled(PreviewFooter)`
-  border-top: 1px solid ${transparentize(0.9, colors.black)};
-  margin-top: 10px;
-  text-align: center;
-  padding-top: 5px;
 `;
