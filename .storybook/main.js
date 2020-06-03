66313adb027b6c8b2005d35f417b5961faab86a8
srdjanRakic <srgjan.rakic@gmail.com>
2020-06-03 08:03:35 +0200

    chore: remove custom storybook webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,26 +1,4 @@
-const path = require('path');
-
-// app's webpack.config.js
-const customWebpackConfig = require('../webpack.config.js');
-
 module.exports = {
   stories: ['../src/**/*.stories.(tsx|mdx)'],
   addons: ['@storybook/addon-docs'],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      use: [
-        {
-          loader: require.resolve('babel-loader'),
-        },
-      ],
-    });
-
-    customWebpackConfig.resolve.extensions.push('.ts', '.tsx');
-
-    return {
-      ...config,
-      resolve: customWebpackConfig.resolve,
-    };
-  },
 };
