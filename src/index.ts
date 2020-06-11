c992395cc6dd17b77af84143ad729b8781469a79
srdjanRakic <srgjan.rakic@gmail.com>
2020-06-11 11:29:52 +0200

    chore: add all exports in index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,44 @@
 // components
-export * from './components';
+export { default as Grid } from './components/Grid';
+export { default as Accordion } from './components/Accordion';
+export { default as Avatar } from './components/Avatar';
+export { default as Button } from './components/Button';
+export { default as Card } from './components/Card';
+export { default as DatePicker } from './components/DatePicker';
+export { default as DayPicker } from './components/DayPicker';
+export { default as Dropdown } from './components/Dropdown';
+export { default as Input } from './components/Input';
+export { default as Label } from './components/Label';
+export { default as Loader } from './components/Loader';
+export { default as Notification } from './components/Notification';
+export { default as PillLabel } from './components/PillLabel';
+export { default as Progress } from './components/Progress';
+export { default as StepWizard } from './components/StepWizard';
+export { default as Switch } from './components/Switch';
+export { default as TextArea } from './components/TextArea';
+export { default as Typography } from './components/Typography';
+export { default as Tabs } from './components/Tabs';
+export { default as Checkbox } from './components/Checkbox';
+export { default as Tooltip } from './components/Tooltip';
+export { default as Note } from './components/Note';
+export { default as Navbar } from './components/NavBar';
+export { default as UserMenu } from './components/UserMenu';
+export { default as TabNavigation } from './components/TabNavigation';
+export { default as Header } from './components/Header';
+export { default as Table } from './components/Table';
+export { default as Modal } from './components/Modal';
+export { default as Radio } from './components/Radio';
+export { default as Pagination } from './components/Pagination';
 
 // previews
-export * from './paywall-previews';
+export { default as Preview1 } from './paywall-previews/Preview1';
+export { default as Preview2 } from './paywall-previews/Preview2';
+export { default as Preview3 } from './paywall-previews/Preview3';
+export { default as Preview4 } from './paywall-previews/Preview4';
+export { default as Preview5 } from './paywall-previews/Preview5';
+export { default as Preview6 } from './paywall-previews/Preview6';
+export { default as Preview7 } from './paywall-previews/Preview7';
+
 
 // theme
 export { default as ThemeWrapper } from './theme/ThemeWrapper';
