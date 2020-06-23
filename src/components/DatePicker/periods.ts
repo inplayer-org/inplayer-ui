b70cd98e8b8c0bed08fa6af51b4f35a3ae962d7d
mustran <musli.ademi@inplayer.com>
2020-06-23 15:54:59 +0200

    Add date range periods

--- src/components/DatePicker/periods.ts
+++ src/components/DatePicker/periods.ts
@@ -1,11 +1,9 @@
 export const PERIODS = {
-  THIS_WEEK: 'this week',
-  LAST_WEEK: 'last week',
-  LAST_TWO_WEEKS: 'last 2 weeks',
-  THIS_MONTH: 'this month',
-  LAST_MONTH: 'last month',
-  THIS_YEAR: 'this year',
-  LAST_SIX_MONTHS: 'last 6 months',
-  ALL_TIME: 'all time',
-  CUSTOM: 'custom date preset',
+  TODAY: '1D',
+  ONE_WEEK: '1W',
+  TWO_WEEKS: '2w',
+  ONE_MONTH: '1M',
+  SIX_MONTHS: '6M',
+  ONE_YEAR: '1Y',
+  ALL: 'ALL',
 };
