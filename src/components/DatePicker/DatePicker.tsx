e1bd9bdfe804f2eb6d1da3e661f0e20c7d710963
mustran <musli.ademi@inplayer.com>
2020-06-23 21:48:11 +0200

    add option to show inner presets on the datepicker

--- src/components/DatePicker/DatePicker.tsx
+++ src/components/DatePicker/DatePicker.tsx
@@ -6,7 +6,7 @@ import colors from '../../theme/colors';
 import { getMonthOptions, getYearOptions } from '../../utils/helpers';
 import Dropdown from '../Dropdown';
 import DatePickerWrapper from './DatePickerWrapper';
-import { PERIODS } from './periods';
+import { PERIODS, INNERPERIODS } from './periods';
 import { Styled } from './styles';
 import 'react-dates/initialize';
 
@@ -59,6 +59,17 @@ interface DateChangeArgs {
   endDate: Moment | null;
 }
 
+type Period =
+  | 'this week'
+  | 'last week'
+  | 'last 2 weeks'
+  | 'this month'
+  | 'last month'
+  | 'last 6 months'
+  | 'this year'
+  | 'all time'
+  | 'custom date preset';
+
 export interface RenderMonthElementProps {
   month: Moment;
   onMonthSelect: (currentMonth: Moment, newMonthVal: string) => void;
@@ -99,19 +110,25 @@ type Props = {
   customAllTimeDate?: Moment;
   activePeriodPreset?: string;
   /**
-   * Boolean indicating whether to show presets or not
+   * Boolean indicating whether to show presets on the left or not
    */
   showPresets?: boolean;
+  /**
+   * Boolean indicating whether to show inner presets or not
+   */
+  showInnerPresets?: boolean;
 };
 
 const DatePicker = ({
   activePeriodPreset = '',
   onDateChange,
   showPresets = false,
+  showInnerPresets = false,
   startDate: startDateProp,
   endDate: endDateProp,
   startDateId = 'startDate',
   endDateId = 'endDate',
+  displayPresets = ['default'],
   isOutsideRange = () => false,
   onFocusChange,
   focusedInput,
@@ -128,21 +145,29 @@ const DatePicker = ({
     let endDate = moment().endOf('day');
     let interval = 'day';
 
+    // PERIODS -> presets shown on the left side of the datepicker (past presets)
+    // Used mostly in analytics
+
+    // INNERPERIODS -> presets shown inside datepicker (future and past presets)
     switch (period) {
       case PERIODS.TODAY:
         startDate = moment().startOf('day');
         endDate = moment().endOf('day');
         break;
       case PERIODS.ONE_WEEK:
+      case INNERPERIODS.LAST_WEEK:
         startDate = startDate.subtract(7, 'days');
         break;
       case PERIODS.TWO_WEEKS:
+      case INNERPERIODS.LAST_TWO_WEEKS:
         startDate = startDate.subtract(14, 'days');
         break;
       case PERIODS.ONE_MONTH:
+      case INNERPERIODS.LAST_MONTH:
         startDate = startDate.subtract(1, 'months');
         break;
       case PERIODS.SIX_MONTHS:
+      case INNERPERIODS.LAST_SIX_MONTHS:
         startDate = startDate.subtract(6, 'months');
         interval = 'month';
         break;
@@ -151,8 +176,21 @@ const DatePicker = ({
         interval = 'month';
         break;
       case PERIODS.ALL:
+      case INNERPERIODS.ALL_TIME:
         startDate = startDate.subtract(2, 'year');
         interval = 'month';
+        break;
+      case INNERPERIODS.THIS_WEEK:
+        startDate = moment().endOf('day');
+        endDate = moment().add(7, 'days');
+        break;
+      case INNERPERIODS.THIS_MONTH:
+        startDate = moment().endOf('day');
+        endDate = moment().add(1, 'month');
+        break;
+      case INNERPERIODS.THIS_YEAR:
+        startDate = moment().endOf('day');
+        endDate = moment().add(1, 'year');
         break;
       default:
         break;
@@ -165,6 +203,36 @@ const DatePicker = ({
     onPeriodChange(period);
   };
 
+  const renderDatePresets = () => {
+    if (!showInnerPresets) return '';
+
+    let presets = [];
+
+    if (displayPresets[0] === 'default') {
+      presets = [
+        INNERPERIODS.THIS_WEEK,
+        INNERPERIODS.LAST_WEEK,
+        INNERPERIODS.THIS_MONTH,
+        INNERPERIODS.LAST_MONTH,
+        INNERPERIODS.THIS_YEAR,
+      ];
+    } else {
+      presets = [...displayPresets];
+    }
+    return (
+      <Styled.DatePresetWrapper>
+        {presets.map((text: string) => (
+          <Styled.StyledLabel
+            active={activePeriod === text}
+            key={text}
+            onClick={() => handleRangeClick(text as Period)}
+          >
+            {text}
+          </Styled.StyledLabel>
+        ))}
+      </Styled.DatePresetWrapper>
+    );
+  };
   const renderMonthElement = ({ month, onMonthSelect, onYearSelect }: RenderMonthElementProps) => (
     <Styled.CustomMonthContainer>
       <Styled.DropdownContainer>
@@ -231,6 +299,7 @@ const DatePicker = ({
               minimumNights={minimumNights}
               enableOutsideDays
               readOnly
+              renderCalendarInfo={renderDatePresets}
             />
           </DatePickerWrapper>
         </DatePickerContainer>
