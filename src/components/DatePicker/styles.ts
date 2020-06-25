5c28e450180be8851fe1910a3ce9b38620497845
mustran <musli.ademi@inplayer.com>
2020-06-25 14:02:06 +0200

    fix padding

--- src/components/DatePicker/styles.ts
+++ src/components/DatePicker/styles.ts
@@ -20,7 +20,7 @@ const StyledLabel = styled(Label)<{ active: boolean }>`
 const DatePresetWrapper = styled.div`
   width: 96px;
   text-align: center;
-  padding: 9rem 0 0.625rem 1rem;
+  padding: 5.5rem 0 0.625rem 1rem;
 `;
 
 const CustomMonthContainer = styled.div`
