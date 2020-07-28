36476f1fec2e552514f42466e071d2a1c51c55c7
Ljubomir Ignov <ljubomir_ignov@hotmail.com>
2020-07-28 14:40:15 +0200

    Create custom toggle switch component

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -1,6 +1,6 @@
 import React, { ChangeEvent } from 'react';
 import styled from 'styled-components';
-import Toggle from 'react-toggle';
+import colors from '../../theme/colors';
 import SwitchWrapper from './SwitchWrapper';
 import Label from '../Label';
 
@@ -29,10 +29,61 @@ type SwitchProps = {
 
 const MarginLeftLabel = styled(Label)`
   margin-right: 0.5rem;
+  position: relative;
+  display: flex;
+  width: fit-content;
+  height: 21px;
+  line-height: 21px;
+`;
+
+const Span = styled.span`
+  margin-right: 1rem;
+`;
+
+const Input = styled.input`
+  opacity: 0;
+  width: 0;
+  height: 0;
+`;
+
+const Slider = styled.span`
+  position: relative;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: ${colors.darkGray};
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  border-radius: 34px;
+  width: 46px;
+  height: 1rem;
+  margin: auto 0;
+  ${Input}:checked + & {
+    background-color: ${colors.skyBlue};
+    ::before {
+      transform: translateX(24px);
+    }
+  }
+
+  ::before {
+    position: absolute;
+    content: '';
+    height: 21px;
+    width: 21px;
+    left: 0px;
+    bottom: -4px;
+    background-color: white;
+    transition: 0.4s;
+    border-radius: 50%;
+    border: 1px solid ${colors.gray};
+  }
 `;
 
 const Switch = ({ id, checked, label, disabled = false, onChange }: SwitchProps) => {
   const onToggleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target, e.target.checked);
     if (e.target) {
       onChange(e.target.checked);
     }
@@ -41,15 +92,16 @@ const Switch = ({ id, checked, label, disabled = false, onChange }: SwitchProps)
   return (
     <SwitchWrapper>
       <MarginLeftLabel disabled={disabled} htmlFor={id}>
-        {label}
+        <Span>{label}</Span>
+        <Input
+          type="checkbox"
+          id={id}
+          disabled={disabled}
+          checked={checked}
+          onChange={onToggleChange}
+        />
+        <Slider />
       </MarginLeftLabel>
-      <Toggle
-        id={id}
-        disabled={disabled}
-        checked={checked}
-        onChange={onToggleChange}
-        icons={false}
-      />
     </SwitchWrapper>
   );
 };
