76ddb2a5da70c9dac4613364907fea358377bddc
srdjanRakic <srgjan.rakic@gmail.com>
2020-08-11 17:10:12 +0200

    chore: revert babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+    '@babel/preset-typescript',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-transform-classes',
+    '@babel/plugin-proposal-private-methods',
+    'babel-plugin-styled-components',
+  ],
+};
