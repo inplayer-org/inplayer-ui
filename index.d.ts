cfe983b1079358f20919fcc0e540f5bdf08a1e1f
mustran <musli.ademi@inplayer.com>
2022-01-20 13:57:46 +0100

    fix tab navigation type on tagging

--- index.d.ts
+++ index.d.ts
@@ -98,6 +98,7 @@ export enum AnalyticsComponentType {
   DAYPICKER = 'daypicker',
   DATEPICKER_PRESET = 'datepicker_preset',
   TAB = 'tab',
+  TAB_NAVIGATION = 'tab_navigation',
 }
 
 export interface Event {
